9c4c7638bc15d462830f7d9dd6446238ef3c7d9c
William Duckitt <william.duckitt@gmail.com>
2019-07-09 13:37:56 +0200

    authentication from the json

--- pvServer/userAuthentication/authenticate.py
+++ pvServer/userAuthentication/authenticate.py
@@ -37,12 +37,32 @@ def loadFileUsers():
     except:
         print("Error Cant load file USERS")
         return None
-
-def loadPvAccessList():
+def createJTWUserIDs(UAGS):
     try:
-        print("loadPvAccessList")
-        with open('userAuthentication/pvList.json') as json_file:
+        users=UAGS['users']
+        #print(users)
+        timestamp=UAGS['timestamp']
+        knownUsers={}
+        for userid in users:
+
+                JWTid=str(jwt.encode({'id':str(userid)+str(timestamp)}, SECRET_PWD_KEY, algorithm='HS256').decode('utf-8'))
+                #print(str(userid) +" :" +str(JWTid))
+                JWTUsernameAndPw=str(jwt.encode({'username':str(userid['username']),'password':str(userid['password'])}, SECRET_PWD_KEY, algorithm='HS256').decode('utf-8'))
+                knownUsers[JWTid]={'JWTUsernameAndPw':JWTUsernameAndPw,'username':userid['username']}
+#                print('user-id: '+userId +' email: '+email+' PW:'+pwd)
+        #print("createJTWUserIDs users: " +str(knownUsers))
+        return knownUsers
+    except:
+        print("Error Cant load file USERS")
+        return None
+
+def loadUsersAndGroups():
+    try:
+        path='userAuthentication/pvList.json'
+        timestamp=os.path.getmtime(path)
+        with open(path) as json_file:
             data = json.load(json_file)
+            data['timestamp']=str(timestamp)
             return data
     except:
         print("Error Cant load file pvAccessList")
@@ -51,19 +71,23 @@ def loadPvAccessList():
 
 REACT_APP_DisableLogin=not(os.getenv('REACT_APP_EnableLogin')=='true')
 if (not REACT_APP_DisableLogin) :
-    knownUsers=loadFileUsers()
-    UAGS=loadPvAccessList()
+    UAGS=loadUsersAndGroups()
+    knownUsers=createJTWUserIDs(UAGS)
+    #knownUsers=loadFileUsers()
+    print(knownUsers)
+    print(UAGS)
 
 
-def checkPermissions(pvname):
+def checkPermissions(pvname,username):
     #print("Checking permissions")
     global UAGS
     d={'read':False,'write':False}
-    for rules in UAGS['UAG1']:
-        match=re.search(str(rules['rule']),str(pvname))
-        if (match):
-            #print(str(pvname)+" :"+str(rules['rule'])+" : "+ str(True))
-            d= {'read':rules['read'],'write':rules['write']}
+    for uag in list(UAGS['userGroups'].keys()):
+        for rules in UAGS['userGroups'][uag]['rules']:
+            match=re.search(str(rules['rule']),str(pvname))
+            if (match):
+                #print(str(pvname)+" :"+str(rules['rule'])+" : "+ str(True))
+                d= {'read':rules['read'],'write':rules['write']}
     return d
             #print(str(pvname)+" :"+str(rules['rule'])+" : "+ str(False))
 
@@ -75,11 +99,12 @@ def authenticateUserAndPermissions(JWT,pvname):
     #JWT=message['authentication']
 #    print('authenticateUser: ',JWT)
     try:
-        decoded_jwt=jwt.decode(str(JWT), SECRET_KEY, algorithms=['HS256'])
+
         #print(decoded_jwt)
-        if decoded_jwt['user-id'] in knownUsers:
+        if JWT in knownUsers:
+            username=knownUsers[JWT]['username']
 #            print("match")
-            permissions=checkPermissions(pvname)
+            permissions=checkPermissions(pvname,username)
             #print(pvname+" :"+ str(permissions))
             d={'authenticated':True,'permissions':permissions}
             return d
@@ -95,9 +120,8 @@ def authenticateUser(JWT):
     global knownUsers
 #    print('authenticateUser: ',JWT)
     try:
-        decoded_jwt=jwt.decode(str(JWT), SECRET_KEY, algorithms=['HS256'])
         #print(decoded_jwt)
-        if decoded_jwt['user-id'] in knownUsers:
+        if JWT in knownUsers:
 #            print("match")
             return True
         else:
@@ -106,23 +130,20 @@ def authenticateUser(JWT):
     except:
         return False
     #print(user)
-def generateJWT(userId):
-    JWT=jwt.encode({'user-id':userId}, SECRET_KEY, algorithm='HS256').decode('utf-8')
-#    print('generateJWT: ',JWT)
-    return JWT
 
 
 def authoriseUser(user):
     global knownUsers
     if knownUsers!= None:
-        JWT=str(jwt.encode({'email':str(user['email']),'password':str(user['password'])}, SECRET_PWD_KEY, algorithm='HS256').decode('utf-8'))
-
-
-        if JWT in knownUsers:
-            if str(user['password'])==knownUsers[JWT]['password']:
-                return generateJWT(JWT)
-            else:
-                return None
+        JWTUsernameAndPw=str(jwt.encode({'username':str(user['email']),'password':str(user['password'])}, SECRET_PWD_KEY, algorithm='HS256').decode('utf-8'))
+        print("JWTUsernameAndPw: "+str(JWTUsernameAndPw))
+        keys=list(knownUsers.keys())
+        print("keys", keys)
+        for JWT in knownUsers:
+            print("JWT", JWT)
+            if JWTUsernameAndPw==knownUsers[JWT]['JWTUsernameAndPw']:
+                return JWT
+            
         else:
             print('Uknown user:' + str(user['email']))
             return None

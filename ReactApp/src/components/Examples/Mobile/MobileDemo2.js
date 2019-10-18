043a73c7ff1f9ec21b298d2cda5bfc0fa3ff26fb
William Duckitt <william.duckitt@gmail.com>
2019-10-18 16:27:32 +0200

    added a tank display

--- ReactApp/src/components/Examples/Mobile/MobileDemo2.js
+++ ReactApp/src/components/Examples/Mobile/MobileDemo2.js
@@ -26,6 +26,7 @@ import RadioButton from '../../BaseComponents/RadioButton';
 import CheckBox from '../../BaseComponents/CheckBox';
 import ThumbWheel from '../../BaseComponents/ThumbWheel';
 import Gauge from '../../BaseComponents/Gauge';
+import Tank from '../../BaseComponents/Tank';
 import StyledIconIndicator from '../../BaseComponents/StyledIconIndicator';
 import lime from '@material-ui/core/colors/lime';
 import SideBar from '../../SystemComponents/SideBar';
@@ -104,6 +105,12 @@ class MobileDemo2 extends React.Component {
             </Grid>
             <Grid item xs={12} sm={6} lg={2}>
               <Gauge  pv='pva://$(device):test3' macros={{'$(device)':'testIOC'}}   usePvLabel={true} usePrecision={true} prec={3} min={-10000} max={10000} />
+            </Grid>
+            <Grid item xs={12} sm={6} lg={1}>
+              <Tank  pv='pva://$(device):test3' macros={{'$(device)':'testIOC'}}   usePvLabel={true} usePrecision={true} prec={3} min={-10000} max={10000} />
+            </Grid>
+            <Grid item xs={12} sm={6} lg={1}>
+                <Tank  pv='pva://$(device):amplitude' macros={{'$(device)':'testIOC'}}   usePvLabel={true} usePrecision={true} prec={3} usePvMinMax={true} alarmSensitive={true}/>
             </Grid>
 
             <Grid item xs={6} lg={2}>

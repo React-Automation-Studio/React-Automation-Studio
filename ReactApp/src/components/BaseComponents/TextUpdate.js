2a8330b83f965463fa0a690133df558e2b16b754
William Duckitt <william.duckitt@gmail.com>
2019-11-18 16:52:08 +0200

    added missing property description

--- ReactApp/src/components/BaseComponents/TextUpdate.js
+++ ReactApp/src/components/BaseComponents/TextUpdate.js
@@ -380,7 +380,10 @@ TextUpdate.propTypes = {
   /** If defined, then the string representaion of the number can be formatted using the mathjs format function eg.  numberFormat={{notation: 'engineering',precision: 3}}. See https://mathjs.org/docs/reference/functions/format.html for more examples*/
   numberFormat:PropTypes.object,
   /** local variable intialization value*/
-  intialLocalVariableValue:PropTypes.string
+  intialLocalVariableValue:PropTypes.string,
+  /** If defined, then the string value of the EPICS enumerator type will be forced to be used, if not defined the the enumerator index is used */
+  useStringValue:PropTypes.bool
+
 
 };
 

7943f24132a14f83cb016e63957869ad98cbc595
William Duckitt <william.duckitt@gmail.com>
2019-11-28 14:28:05 +0200

    Updated to MUI-slider  with new features

--- ReactApp/src/components/BaseComponents/SimpleSlider.js
+++ ReactApp/src/components/BaseComponents/SimpleSlider.js
@@ -31,13 +31,18 @@ const styles  = theme =>({
     display: 'flex',
     justifyContent: 'space-between',
     paddingTop: theme.spacing(1) * 2
+  },
+  sliderDiv: {
+
+    paddingRight: theme.spacing(1)*3,
+    paddingLeft: theme.spacing(1)*3
   }
 });
 /**
  * The SimpleSlider Component is a wrapper on the Material-UI contained Slider component. The SimpleSlider component is implemented with zero margins and enabled to grow to the width of its parent container.<br/><br/>
  * The margins and spacing must be controlled from the parent component.<br/><br/>
  * Material-UI Slider Demos:
- * https://material-ui.com/slider/<br/><br/>
+ * https://material-ui.com/components/slider/<br/><br/>
  * Material-UI Slider API:
  * https://material-ui.com/api/slider/
 
@@ -61,7 +66,9 @@ class SimpleSlider extends React.Component {
   this.handleOnDragEnd=this.handleOnDragEnd.bind(this);
   this.handleOnDragStart=this.handleOnDragStart.bind(this);
   this.handleChange = this.handleChange.bind(this);
+  this.handleChangeCommited = this.handleChangeCommited.bind(this);
   this.emitChangeDebounced = debounce(this.emitChange, 10);
+  this.valuetext=this.valuetext.bind(this);
 }
 
 
@@ -76,7 +83,8 @@ handleInputValue(inputValue,pvname,initialized,severity){
     ['initialized']:initialized,
     ['severity']:severity});
   }
-  else {  this.setState({['inputValue']:inputValue,
+  else {  this.setState(
+  {['inputValue']:inputValue,
   ['pvname']:pvname,
   ['initialized']:initialized,
   ['severity']:severity});
@@ -153,13 +161,22 @@ emitChange(value) {
 }
 
 handleChange = (event, value) => {
-  this.emitChangeDebounced(value);
+  //console.log("handleChange")
+  this.setState({hasFocus:true,outputValue:value});
+  //this.emitChangeDebounced(value);
+};
+
+handleChangeCommited = (event, value) => {
+  this.setState({hasFocus:false,outputValue:value,inputValue:value});
+
+  //console.log("onChangeCommitted")
+  //this.emitChangeDebounced(value);
 
 };
 
-
-
-
+valuetext= value =>()=> {
+ return `${value}`;
+}
 
 
 render() {
@@ -190,7 +207,7 @@ render() {
 
     }
     else {
-      units=this.props.units;
+      units=typeof this.props.units!=='undefined'?this.props.units:"";
     }
 
     if (value!==""){
@@ -298,12 +315,13 @@ render() {
       />
 
       {initialized===true &&
-        <div >
+        <div className={classes.sliderDiv}>
           <div className={classes.rangeLabel}>
-            <Typography id="subtitle2">{usePvLabel===true? this.state['label']+": ":this.props.label} {+this.state.outputValue}</Typography>
+            <Typography id="subtitle2">{usePvLabel===true? this.state['label']+": ":this.props.label+" "+units} {+this.state.outputValue+" "+units}</Typography>
           </div>
 
           <Slider
+
             className={classes.slider}
             disabled={disabled}
             value={this.state.outputValue}
@@ -313,28 +331,19 @@ render() {
             onDragEnd={this.handleOnDragEnd}
             max={max}
             min={min}
-            step={typeof this.props.step !== 'undefined'?this.props.step:undefined}
+            marks={typeof this.props.marks!=='undefined'?this.props.marks:[{value:min,label:min+" "+units},{value:max,label:max+" "+units}]}
+            valueLabelDisplay={this.props.showThumbValue===true?"on":undefined}
 
+            step={typeof this.props.step !== 'undefined'?this.props.step:undefined}
+            onChangeCommitted={this.handleChangeCommited}
 
           />
-          <div className={classes.rangeLabel}>
-            <div>
-              <Typography variant="subtitle2">
-                {min}
-              </Typography>
-            </div>
-            <div>
-              <Typography variant="subtitle2">
-                {max}
-              </Typography>
-            </div>
-          </div>
 
         </div>
       }
 
       {(initialized===false||initialized==='undefined') &&
-        <div>
+        <div className={classes.sliderDiv}>
           <div className={classes.rangeLabel}>
             <Typography id="subtitle2">{<span> <LanDisconnect style={{color:this.props.theme.palette.error.main,verticalAlign: "middle"}} fontSize='small'/> {this.state['pvname']} </span> }</Typography>
 
@@ -346,26 +355,15 @@ render() {
             value={this.state.outputValue}
             aria-labelledby="label"
             onChange={this.handleChange}
-            onDragStart={this.handleOnDragStart}
-            onDragEnd={this.handleOnDragEnd}
+
+
             max={max}
             min={min}
             step={typeof this.props.step !== 'undefined'?this.props.step:undefined}
 
 
           />
-          <div className={classes.rangeLabel}>
-            <div>
-              <Typography variant="subtitle2">
-                {'N/A'}
-              </Typography>
-            </div>
-            <div>
-              <Typography variant="subtitle2">
-                {'N/A'}
-              </Typography>
-            </div>
-          </div>
+
 
         </div>
       }
@@ -394,7 +392,10 @@ render() {
         min:PropTypes.number,
         /** Custom maximum to be used */
         max:PropTypes.number,
-
+        /** custom markers: in format : [{value: uservalue1,label:userlabel1},{value: uservalue...,label:userlabel...}*/
+        marks: PropTypes.array,
+        /** show thumb with value */
+        showThumbValue:PropTypes.bool,
         /** If defined, then the DataConnection debugging information will be displayed*/
         debug:PropTypes.bool,
         /** If defined, the value will be increment or decremented in the define step intervals*/
@@ -408,8 +409,9 @@ render() {
       SimpleSlider.defaultProps = {
 
           debug:false,
-          step:0.1,
-
+          step:1,
+          marks:undefined,
+          showThumbValue:false
 
       };
 

705d53606b6421773c4ed1f2cdf36c79690e0b6d
William Duckitt <william.duckitt@gmail.com>
2020-02-19 11:48:47 +0200

    updated to version 1.2.2

--- pvServer/pvServer.py
+++ pvServer/pvServer.py
@@ -30,7 +30,7 @@ load_dotenv()
 async_mode = 'gevent'
 print("")
 print('**************************************')
-print("React Automation Studio V1.2.0")
+print("React Automation Studio V1.2.2")
 print("")
 print("pvServer Environment Variables:")
 print("")

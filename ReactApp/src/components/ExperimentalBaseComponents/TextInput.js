3bdbbb3676bd30c261f0ae8f5c4c0d099675e2dc
William Duckitt <william.duckitt@gmail.com>
2020-04-30 09:54:13 +0200

    updated to GenericWidget

--- ReactApp/src/components/ExperimentalBaseComponents/TextInput.js
+++ ReactApp/src/components/ExperimentalBaseComponents/TextInput.js
@@ -3,16 +3,119 @@ import { withStyles } from "@material-ui/core/styles";
 import { InputAdornment, TextField } from "@material-ui/core";
 import PropTypes from 'prop-types';
 import GenericWidget from "../SystemComponents/Widgets/GenericWidget";
+import red from '@material-ui/core/colors/red';
+import deepOrange from '@material-ui/core/colors/deepOrange';
+
+
 const styles = (theme) => ({
   root: {
     display: "flex",
     flexWrap: "wrap",
   },
-  textField: {
-    width: "100%",
+  TextFieldSeverity0: {
+    width: '100%',
     borderRadius: 4,
   },
+  TextFieldSeverity1: {
+    width: '100%',
+    borderRadius: 4,
+    background: 'linear-gradient(45deg, ' + theme.palette.background.default + ' 1%, ' + deepOrange['400'] + ' 99%)'
+  },
+  TextFieldSeverity2: {
+    width: '100%',
+    borderRadius: 4,
+    background: 'linear-gradient(45deg, ' + theme.palette.background.default + ' 1%, ' + red['800'] + ' 99%)'
+  }
 });
+
+function TextInputComponent(props) {
+
+  /**
+   * Temporary store the new value
+   * @param {Event} event
+   */
+  function handleChange(event) {
+    props.onUpdateWidgetState({ value: event.target.value });
+  }
+
+  /**
+   * Save value on output value only when 'Enter' key is pressed.
+   * @param {Event} event
+   */
+  function catchReturn(event) {
+    if (event.key === "Enter" && props.connection) {
+      props.onUpdateWidgetState({
+        checkValue: true,
+        value: props.value,
+        outputValue: props.value,
+      });
+    }
+  }
+
+
+  let style;
+  const { classes } = props;
+  const { alarmColor } = props;
+  const { connection } = props;
+  const { theme } = props;
+  const { value } = props;
+
+  let textFieldClassName;
+  if (typeof props.alarmSensitive !== 'undefined') {
+    if (props.alarmSensitive == true) {
+      if (props.alarmSeverity == 1) {
+        textFieldClassName = classes.TextFieldSeverity1;
+
+      }
+      else if (props.alarmSeverity == 2) {
+        textFieldClassName = classes.TextFieldSeverity2;
+
+      }
+      else {
+        textFieldClassName = classes.TextFieldSeverity0;
+
+      }
+
+    }
+  }
+
+
+  let inputProps;
+
+  if (connection) {
+    inputProps = {
+      endAdornment: (
+        <InputAdornment position="end">
+          {props.units} {props.children}
+        </InputAdornment>
+      ),
+    };
+  } else {
+    inputProps = { readOnly: true };
+  }
+
+
+  return (
+    <TextField
+      className={textFieldClassName}
+
+      key={props.pvName}
+      //aria-owns={this.state.openContextMenu ? 'menu-list-grow' : undefined}
+      aria-haspopup="true"
+      value={value}
+      onKeyPress={catchReturn}
+      onFocus={props.onUpdateWidgetFocus}
+      onBlur={props.onUpdateWidgetBlur}
+      onChange={handleChange}
+      label={props.label}
+      fullWidth={true}
+      margin="none"
+      variant={props.variant}
+      disabled={props.disabled}
+      InputProps={inputProps}
+    />
+  );
+}
 
 /**
  * The TextInput Component is a wrapper on the Material-UI contained TextField component.
@@ -22,85 +125,21 @@ const styles = (theme) => ({
  * https://material-ui.com/demos/text-fields<br/><br/>
  * Material-UI TextField API:
  * https://material-ui.com/api/text-field
+ * 
+ * 
+ * 
  */
 class TextInput extends React.Component {
   constructor(props) {
     super(props);
-    this.catchReturn = this.catchReturn.bind(this);
-    this.handleChange = this.handleChange.bind(this);
   }
-
-  /**
-   * Temporary store the new value
-   * @param {Event} event
-   */
-  handleChange=(widgetProps)=>(event)=> {
-    widgetProps.onUpdateWidgetState({ value: event.target.value });
-  }
-
-  /**
-   * Save value on output value only when 'Enter' key is pressed.
-   * @param {Event} event
-   */
-  catchReturn=(widgetProps)=>(event)=> {
-    if (event.key === "Enter" && widgetProps.connection) {
-      widgetProps.onUpdateWidgetState({
-        checkValue: true,
-        value:widgetProps.value,
-        outputValue: widgetProps.value,
-      });
-    }
-  }
-
   render() {
-    const { classes } = this.props;
-    let inputProps;
-    let style;
-
     return (
       <GenericWidget {...this.props}>
         {(widgetProps) => {
-          if (widgetProps.alarmColor !== "") {
-            style = {
-              background:
-                "linear-gradient(45deg, " +
-                this.props.theme.palette.background.default +
-                " 1%, " +
-                widgetProps.alarmColor +
-                " 99%)",
-            };
-          }
-          if (widgetProps.connection) {
-            inputProps = {
-              endAdornment: (
-                <InputAdornment position="end">
-                  {this.props.units} {this.props.children}
-                </InputAdornment>
-              ),
-            };
-          } else {
-            inputProps = { readOnly: true };
-          }
           return (
-            <TextField
-              className={classes.textField}
-              style={style}
-              key={widgetProps.pvName}
-              //aria-owns={this.state.openContextMenu ? 'menu-list-grow' : undefined}
-              aria-haspopup="true"
-              value={widgetProps.value}
-              onKeyPress={this.catchReturn(widgetProps)}
-              onFocus={widgetProps.onUpdateWidgetFocus}
-              onBlur={widgetProps.onUpdateWidgetBlur}
-              onChange={this.handleChange(widgetProps)}
-              label={widgetProps.label}
-              fullWidth={true}
-              margin="none"
-              variant="outlined"
-              disabled={widgetProps.disabled}
-              InputProps={inputProps}
-            />
-          );
+            <TextInputComponent {...this.props} {...widgetProps} />
+          )
         }
         }
       </GenericWidget>
@@ -112,37 +151,45 @@ TextInput.propTypes = {
   /** Name of the process variable, NB must contain correct prefix ie: pva://  eg. 'pva://$(device):test$(id)'*/
   pv: PropTypes.string.isRequired,
   /** Values of macros that will be substituted in the pv name eg. {{'$(device)':'testIOC','$(id)':'2'}}*/
-  macros:PropTypes.object,
+  macros: PropTypes.object,
   /** Directive to fill the label with the value contained in the  EPICS pv's DESC field. */
-  usePvLabel:PropTypes.bool,
+  usePvLabel: PropTypes.bool,
   /** Directive to use the units contained in the  EPICS pv's EGU field. */
   usePvUnits: PropTypes.bool,
   /** Directive to round the value. */
-  usePrecision:PropTypes.bool,
+  usePrecision: PropTypes.bool,
   /** Custom precision to round the value too, if not defined then the EPICS PREC field will be used, if `usePrecision` is defined. */
-  prec:PropTypes.number,
+  prec: PropTypes.number,
   /** Custom units to be used, if `usePvUnits` is not defined. */
-  units:PropTypes.string,
+  units: PropTypes.string,
   /** Directive to use the HOPR and LOPR EPICS fields to limit the maximum and minimum values that can be contained in the value. */
-  usePvMinMax:PropTypes.bool,
+  usePvMinMax: PropTypes.bool,
   /** Directive to use the EPICS alarm severity status to alter the fields backgorund color  */
-  alarmSensitive:PropTypes.bool,
+  alarmSensitive: PropTypes.bool,
   /** Custom label to be used, if  `usePvLabel` is not defined. */
   label: PropTypes.string,
   /** Custom minimum to be used, if `usePvMinMax` is not defined. */
-  min:PropTypes.number,
+  min: PropTypes.number,
   /** Custom maximum to be used, if `usePvMinMax` is not defined. */
-  max:PropTypes.number,
+  max: PropTypes.number,
   /** If defined, then the string value of the EPICS enumerator type will be forced to be used, if not defined the the enumerator index is used */
-  useStringValue:PropTypes.bool,
+  useStringValue: PropTypes.bool,
   /** If defined, then the DataConnection debugging information will be displayed*/
-  debug:PropTypes.bool,
+  debug: PropTypes.bool,
   /** local variable intialization value*/
-  intialLocalVariableValue:PropTypes.string
+  intialLocalVariableValue: PropTypes.string,
+  /** Material-UI TextField variant*/
+  variant: PropTypes.string,
+  /** Material-UI TextField margin*/
+  margin: PropTypes.string,
+
 
 };
-TextInput.defaultProps={
-  debug:false
+TextInput.defaultProps = {
+  debug: false,
+  variant: "outlined",
+  margin: "none",
+  alarmSensitive: false
 };
 
 export default withStyles(styles, { withTheme: true })(TextInput);
--- ReactApp/src/components/ExperimentalBaseComponents/TextOutput.js
+++ ReactApp/src/components/ExperimentalBaseComponents/TextOutput.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { withStyles } from "@material-ui/core/styles";
 import { InputAdornment, TextField } from "@material-ui/core";
-
+import PropTypes from 'prop-types';
 import GenericWidget from "../SystemComponents/Widgets/GenericWidget";
-
+import red from '@material-ui/core/colors/red';
+import deepOrange from '@material-ui/core/colors/deepOrange';
 const styles = (theme) => ({
   root: {
     display: "flex",
@@ -63,23 +64,24 @@ const styles = (theme) => ({
   bootstrapFormLabel: {
     fontSize: 18,
   },
-  textField: {
-    width: "100%",
-    fontWeight: 500,
+  TextFieldSeverity0: {
+    width: '100%',
     borderRadius: 4,
   },
+  TextFieldSeverity1: {
+    width: '100%',
+    borderRadius: 4,
+    background:'linear-gradient(45deg, '+ theme.palette.background.default+ ' 1%, '+deepOrange['400'] +' 99%)'
+  },
+  TextFieldSeverity2: {
+    width: '100%',
+    borderRadius: 4,
+    background:'linear-gradient(45deg, '+ theme.palette.background.default+ ' 1%, '+red['800'] +' 99%)'
+  }
 });
 
-/**
- *  The TextOutput Component is a wrapper on the Material-UI contained TextField component in read-only mode.
- *  The TextField component is implemented with zero margins and enabled to grow to the width of its parent container.<br/><br/>
- *  The margins and spacing must be controlled from the parent component.<br/><br/>
- *  Material-UI TextField Demos:
- *  https://material-ui.com/demos/text-fields<br/><br/>
- *  Material-UI TextField API:
- *  https://material-ui.com/api/text-field
- */
-function TextOutput(props) {
+
+function TextOutputComponent(props) {
   const { classes } = props;
   let inputProps = {
     classes: {
@@ -97,55 +99,121 @@ function TextOutput(props) {
     },
   };
 
+  let textFieldClassName;
+  if (typeof props.alarmSensitive !== 'undefined') {
+    if (props.alarmSensitive == true) {
+      if (props.alarmSeverity == 1) {
+        textFieldClassName = classes.TextFieldSeverity1;
 
+      }
+      else if (props.alarmSeverity == 2) {
+        textFieldClassName = classes.TextFieldSeverity2;
+
+      }
+      else {
+        textFieldClassName = classes.TextFieldSeverity0;
+
+      }
+
+    }
+  }
   return (
-    <GenericWidget  {...props} readOnly= {true}>
-      {(widgetProps) => {
-        let inputProps = {};
-        if (widgetProps.connection) {
-          inputProps["endAdornment"] = (
-            <InputAdornment position="end">
-              {props.units} {props.children}
-            </InputAdornment>
-          );
-        }
-
-        let style;
-        if (widgetProps.alarmColor !== "") {
-          style = {
-            background:
-              "linear-gradient(45deg, " +
-              props.theme.palette.background.default +
-              " 1%, " +
-              widgetProps.alarmColor +
-              " 99%)",
-          };
-        }
-
-
-        return (
+    
           <TextField
-            className={classes.textField}
-            style={style}
-            key={widgetProps.pvName}
+            className={classes.textFieldClassName}
+           
+            key={props.pvName}
             //aria-owns={state.openContextMenu ? 'menu-list-grow' : undefined}
             aria-haspopup="true"
-            value={widgetProps.value}
+            value={props.value}
             fullWidth={true}
-            onFocus={widgetProps.onUpdateWidgetFocus}
-            onBlur={widgetProps.onUpdateWidgetBlur}
-            label={widgetProps.label}
+            onFocus={props.onUpdateWidgetFocus}
+            onBlur={props.onUpdateWidgetBlur}
+            label={props.label}
             margin="none"
             variant="outlined"
-            disabled={widgetProps.disabled}
+            disabled={props.disabled}
             InputLabelProps={inputLabelProps}
             InputProps={inputProps}
           />
         )
-      }}
+      
 
-    </GenericWidget>
-  );
+   
+  
 }
+
+
+/**
+ *  The TextOutput Component is a wrapper on the Material-UI contained TextField component in read-only mode.
+ *  The TextField component is implemented with zero margins and enabled to grow to the width of its parent container.<br/><br/>
+ *  The margins and spacing must be controlled from the parent component.<br/><br/>
+ *  Material-UI TextField Demos:
+ *  https://material-ui.com/demos/text-fields<br/><br/>
+ *  Material-UI TextField API:
+ *  https://material-ui.com/api/text-field
+ */
+class TextOutput extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <GenericWidget {...this.props}>
+         {(widgetProps) => {
+           return(
+               <TextOutputComponent {...this.props} {...widgetProps}/>
+           )
+         }
+        }
+      </GenericWidget>
+    )
+  }
+}
+
+TextOutput.propTypes = {
+  /** Name of the process variable, NB must contain correct prefix ie: pva://  eg. 'pva://$(device):test$(id)'*/
+  pv: PropTypes.string.isRequired,
+  /** Values of macros that will be substituted in the pv name eg. {{'$(device)':'testIOC','$(id)':'2'}}*/
+  macros: PropTypes.object,
+  /** Directive to fill the label with the value contained in the  EPICS pv's DESC field. */
+  usePvLabel: PropTypes.bool,
+  /** Directive to use the units contained in the  EPICS pv's EGU field. */
+  usePvUnits: PropTypes.bool,
+  /** Directive to round the value. */
+  usePrecision: PropTypes.bool,
+  /** Custom precision to round the value too, if not defined then the EPICS PREC field will be used, if `usePrecision` is defined. */
+  prec: PropTypes.number,
+  /** Custom units to be used, if `usePvUnits` is not defined. */
+  units: PropTypes.string,
+  /** Directive to use the HOPR and LOPR EPICS fields to limit the maximum and minimum values that can be contained in the value. */
+  usePvMinMax: PropTypes.bool,
+  /** Directive to use the EPICS alarm severity status to alter the fields backgorund color  */
+  alarmSensitive: PropTypes.bool,
+  /** Custom label to be used, if  `usePvLabel` is not defined. */
+  label: PropTypes.string,
+  /** Custom minimum to be used, if `usePvMinMax` is not defined. */
+  min: PropTypes.number,
+  /** Custom maximum to be used, if `usePvMinMax` is not defined. */
+  max: PropTypes.number,
+  /** If defined, then the string value of the EPICS enumerator type will be forced to be used, if not defined the the enumerator index is used */
+  useStringValue: PropTypes.bool,
+  /** If defined, then the DataConnection debugging information will be displayed*/
+  debug: PropTypes.bool,
+  /** local variable intialization value*/
+  intialLocalVariableValue: PropTypes.string,
+  /** Material-UI TextField variant*/
+  variant: PropTypes.string,
+  /** Material-UI TextField margin*/
+  margin: PropTypes.string,
+
+
+};
+TextOutput.defaultProps = {
+  debug: false,
+  variant: "outlined",
+  margin: "none",
+  alarmSensitive: false
+};
 
 export default withStyles(styles, { withTheme: true })(TextOutput);

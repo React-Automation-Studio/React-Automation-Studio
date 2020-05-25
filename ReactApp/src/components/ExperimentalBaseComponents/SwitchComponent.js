943134b794981b8a5f54a00cafdceea12815a4eb
William Duckitt <william.duckitt@gmail.com>
2020-05-25 15:53:21 +0200

    updated to hooks widget

--- ReactApp/src/components/ExperimentalBaseComponents/SwitchComponent.js
+++ ReactApp/src/components/ExperimentalBaseComponents/SwitchComponent.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { withStyles } from "@material-ui/core/styles";
 import { FormControlLabel, Switch } from "@material-ui/core";
-import withWidget from "../../SystemComponents/Widgets/withWidget";
+import Widget from "../SystemComponents/Widgets/Widget";
 
 const styles = (theme) => ({
   root: {
@@ -27,45 +27,43 @@ const styles = (theme) => ({
  * Material-UI Switch API:
  * https://material-ui.com/api/switch/
  */
-class SwitchComponent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleButtonChange = this.handleButtonChange.bind(this);
-  }
+const SwitchInternalComponent = (props) => {
+  
 
   /**
    * Save switch state.
    * @param {Event} event
    */
-  handleButtonChange(event) {
+ const handleButtonChange=(event)=> {
     let value = event.target.checked ? 1 : 0;
-    this.props.onUpdateWidgetState({
-      value: value,
-      outputValue: value,
-      newValueTrigger: 1,
-    });
+    props.handleImmediateChange(value);
   }
 
-  render() {
+ 
     return (
       <FormControlLabel
-        key={this.props.pvName}
-        className={this.props.classes.FormControl}
-        disabled={this.props.disabled}
-        label={this.props.label}
-        labelPlacement={this.props.labelPosition}
+        key={props.pvName}
+        className={props.classes.FormControl}
+        disabled={props.disabled}
+        label={props.label}
+        labelPlacement={props.labelPosition}
         control={
           <Switch
-            onChange={this.handleButtonChange}
-            checked={this.props.value === 1}
-            color={this.props.onColor}
+            onChange={handleButtonChange}
+            checked={props.value == 1}
+            color={props.onColor}
           />
         }
       />
     );
   }
-}
 
-export default withWidget(
-  withStyles(styles, { withTheme: true })(SwitchComponent)
-);
+const SwitchComponent =(props)=>{
+  return (
+    <Widget {...props} component={SwitchInternalComponent}/>
+       
+    
+  )
+}
+export default withStyles(styles, { withTheme: true })(SwitchComponent)
+
---
+++ ReactApp/src/components/ExperimentalBaseComponents/SwitchComponent.md
@@ -0,0 +1,34 @@
+
+
+SwitchComponent example connection to a SoftChannel EPICS AI pv with use of EPICS label and a custom label position:
+
+```js
+{/*The TextOuput code is included for demonstration purposes only*/}  
+{/*Only the the JSX code between the hashes  is required to instantiate the SwitchComponent */}  
+  import TextOutput from './TextOutput';
+  <React.Fragment>
+  <div style={{marginBottom:8}}>
+    <TextOutput
+      pv='pva://$(device):BO$(id)'
+      macros={{'$(device)':'testIOC','$(id)':'1'}}
+      usePvLabel={true}
+      usePrecision={true}
+      usePvUnits={true}
+      usePvMinMax={true}
+      alarmSensitive={false}
+      />
+  </div>
+
+
+{/*###############*/}  
+
+  <SwitchComponent
+    pv='pva://$(device):BO$(id)'
+    macros={{'$(device)':'testIOC','$(id)':'1'}}
+    usePvLabel={true}
+    labelPosition={"end"}
+    />
+
+{/*###############*/}  
+  </React.Fragment>
+```

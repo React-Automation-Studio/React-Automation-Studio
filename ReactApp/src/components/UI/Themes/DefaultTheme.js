190b277b7f2e8f56670fbf38332ec3adcb13a68e
William Duckitt <william.duckitt@gmail.com>
2020-06-04 18:53:03 +0200

    editing themes

--- ReactApp/src/components/UI/Themes/DefaultTheme.js
+++ ReactApp/src/components/UI/Themes/DefaultTheme.js
@@ -1,89 +1,144 @@
-import { indigo, pink, red, green, cyan, deepOrange, orange } from '@material-ui/core/colors'
+import { indigo, blueGrey, pink, red, green, cyan, deepOrange, orange,amber } from '@material-ui/core/colors'
 import { fade } from '@material-ui/core/styles/colorManipulator';
 
-const lightPalette = {
-    type: "light",
-    primary: indigo,
-    secondary: pink,
-    error: pink,
-    alarm: {
+const lightPalette =()=> {
+    const type= "light";
+    const primary= indigo;
+    const secondary= pink;
+    const error= pink;
+    const major= red;
+    const minor= deepOrange;
+    const alarm= {
         major: {
-            light: red['400'],
-            main: red['600'],
-            dark: red['700'],
-        },
-        majorAcked: {
-            light: fade(red['400'], 0.4),
-            main: fade(red['600'], 0.4),
-            dark: fade(red['800'], 0.4),
+            light:major['400'],
+            main: major['600'],
+            dark: major['800'],
         },
         minor: {
-            light: orange['100'],
-            main: orange['300'],
-            dark: orange['500']
+            light:minor['100'],
+            main: minor['200'],
+            dark: minor['300']
         },
-        minorAcked: {
-            light: fade(orange['100'], 0.4),
-            main: fade(orange['300'], 0.4),
-            dark: fade(orange['600'], 0.4)
-        },
+     
+    };
 
-        ok: {
-            light: green['200'],
-            dark: green['500']
-        },
-    },
-    // Used by `getContrastText()` to maximize the contrast between the background and
-    // the text.
-    contrastThreshold: 3,
-    // Used to shift a color's luminance by approximately
-    // two indexes within its tonal palette.
-    // E.g., shift from Red 500 to Red 300 or Red 700.
-    tonalOffset: 0.2,
-    paperElevation: 10,
+    alarm.majorAcked= {
+        light: fade(alarm.major.light, 0.4),
+        main:  fade(alarm.major.main, 0.4),
+        dark:  fade(alarm.major.dark, 0.4),
+    };
+    alarm.minorAcked= {
+        light: fade(alarm.minor.light, 0.4),
+        main: fade(alarm.minor.main, 0.4),
+        dark: fade(alarm.minor.dark, 0.4)
+    };
+    const contrastThreshold= 3;
+    const tonalOffset= 0.2;
+    const paperElevation= 10;
+    return({
+        type:type,
+        primary:primary,
+        secondary:secondary,
+        error:error,
+        major:major,
+        minor:minor,
+        alarm:alarm,
+        contrastThreshold:contrastThreshold,
+        tonalOffset:tonalOffset,
+        paperElevation:paperElevation
+
+    })
 }
 
-const darkPalette = {
-    type: "dark",
-    primary: cyan,
-    secondary: pink,
-    error: pink,
-    action: green,
-    // Used by `getContrastText()` to maximize the contrast between the background and
-    // the text.
-    contrastThreshold: 3,
-    alarm: {
+const darkPalette =()=> {
+    const type= "dark";
+    const primary= cyan;
+    const secondary= pink;
+    const error= pink;
+    const major= red;
+    const minor= deepOrange;
+    const alarm= {
         major: {
-            light: red['400'],
-            main: red['600'],
-            dark: red['800'],
-        },
-        majorAcked: {
-            light: fade(red['400'], 0.4),
-            main: fade(red['600'], 0.4),
-            dark: fade(red['800'], 0.4),
+            light:major['400'],
+            main: major['600'],
+            dark: major['800'],
         },
         minor: {
-            light: deepOrange['200'],
-            main: deepOrange['300'],
-            dark: deepOrange['400']
+            light:minor['100'],
+            main: minor['200'],
+            dark: minor['300']
         },
-        minorAcked: {
-            light: fade(deepOrange['200'], 0.4),
-            main: fade(deepOrange['400'], 0.4),
-            dark: fade(deepOrange['500'], 0.4)
-        },
-        ok: {
-            light: green['200'],
-            dark: green['500']
-        },
-    },
-    // Used to shift a color's luminance by approximately
-    // two indexes within its tonal palette.
-    // E.g., shift from Red 500 to Red 300 or Red 700.
-    tonalOffset: 0.2,
-    paperElevation: 1,
+     
+    };
+
+    alarm.majorAcked= {
+        light: fade(alarm.major.light, 0.4),
+        main:  fade(alarm.major.main, 0.4),
+        dark:  fade(alarm.major.dark, 0.4),
+    };
+    alarm.minorAcked= {
+        light: fade(alarm.minor.light, 0.4),
+        main: fade(alarm.minor.main, 0.4),
+        dark: fade(alarm.minor.dark, 0.4)
+    };
+    const contrastThreshold= 3;
+    const tonalOffset= 0.2;
+    const paperElevation= 10;
+    return({
+        type:type,
+        primary:primary,
+        secondary:secondary,
+        error:error,
+        major:major,
+        minor:minor,
+        alarm:alarm,
+        contrastThreshold:contrastThreshold,
+        tonalOffset:tonalOffset,
+        paperElevation:paperElevation
+
+    })
 }
+// const darkPalette = {
+//     type: "dark",
+//     primary: cyan,
+//     secondary: pink,
+//     error: pink,
+//     action: green,
+//     // Used by `getContrastText()` to maximize the contrast between the background and
+//     // the text.
+//     contrastThreshold: 3,
+//     alarm: {
+//         major: {
+//             light: red['400'],
+//             main: red['600'],
+//             dark: red['800'],
+//         },
+//         majorAcked: {
+//             light: fade(red['400'], 0.4),
+//             main: fade(red['600'], 0.4),
+//             dark: fade(red['800'], 0.4),
+//         },
+//         minor: {
+//             light: deepOrange['200'],
+//             main: deepOrange['400'],
+//             dark: deepOrange['500']
+//         },
+//         minorAcked: {
+//             light: fade(deepOrange['200'], 0.4),
+//             main: fade(deepOrange['400'], 0.4),
+//             dark: fade(deepOrange['500'], 0.4)
+//         },
+//         ok: {
+//             light: green['200'],
+//             dark: green['500']
+//         },
+//     },
+//     // Used to shift a color's luminance by approximately
+//     // two indexes within its tonal palette.
+//     // E.g., shift from Red 500 to Red 300 or Red 700.
+//     tonalOffset: 0.2,
+//     paperElevation: 1,
+// }
 
 const themeProps = {
     lightLineColors: ['#12939A', '#79C7E3', '#1A3177', '#FF9833', '#EF5D28'],
@@ -94,11 +149,11 @@ const themeProps = {
 }
 
 export const lightTheme = {
-    palette: { ...lightPalette },
+    palette: { ...lightPalette() },
     ...themeProps
 }
 
 export const darkTheme = {
-    palette: { ...darkPalette },
+    palette: { ...darkPalette() },
     ...themeProps
 }

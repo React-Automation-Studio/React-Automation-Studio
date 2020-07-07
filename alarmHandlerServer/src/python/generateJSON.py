aa9eced4281305d2f4be43d5119a40103e59d585
justinabraham <jusabra@gmail.com>
2020-07-07 10:34:44 +0200

    Modified pvlist and genJSON scripts to be more robust and user friendly.

--- alarmHandlerServer/src/python/generateJSON.py
+++ alarmHandlerServer/src/python/generateJSON.py
@@ -3,8 +3,6 @@ import json
 with open('./initDBData/pvList.json') as f:
     data = json.load(f)
 
-keys = {}
-
 pvs = {}
 pvsFile = []
 
@@ -13,94 +11,70 @@ historyFile = []
 historyFile.append({"id": "_GLOBAL", "history": []})
 
 
-area = ''
-subArea = ''
-areaList = []
-subAreaList = []
+areaName = ''
 
-for entry in data:
-    # area
-    area = entry["pv"].split("=")[0].split("*")[0]
-    if(area not in areaList):
-        # new area
-        areaList.append(area)
-        pvs[area] = {
-            "area": area,
-            "enable": True,
-            "pvs": {}
-        }
-        keys[area] = {
-            "pvKey": 0,
-            "subAreaKey": 0
-        }
-        history[area] = {
-            "id": area,
-            "history": []
-        }
-    if(len(entry["pv"].split("=")) == 1):
-        # area
-        if(len(entry["pv"].split("*")) > 1):
-            # pv
-            pv = entry["pv"].split("*")[1]
-            pvs[area]["pvs"]["pv"+str(keys[area]["pvKey"])] = {
-                "name": pv,
-                "enable": True,
-                "latch": True,
-                "notify": True,
-                "lastAlarmVal": "",
-                "lastAlarmTime": "",
-                "lastAlarmAckTime": ""
-            }
-            keys[area]["pvKey"] += 1
-            history[area+"*"+pv] = {
-                "id": area+"*"+pv,
-                "history": []
-            }
-    else:
-        # subArea
-        subArea = entry["pv"].split("=")[1].split("*")[0]
-        identifier = area+"="+subArea
-        if(identifier not in subAreaList):
-            # new subArea
-            subAreaList.append(identifier)
-            keys[identifier] = {
-                "pvKey": 0
-            }
-            pvs[area]["subArea"+str(keys[area]["subAreaKey"])] = {
-                "name": subArea,
+for area in data:
+    for areaKey, areaValue in area.items():
+        if(areaKey == "area"):
+            # new area template
+            areaName = areaValue
+            pvs[areaName] = {
+                "area": areaName,
                 "enable": True,
                 "pvs": {}
             }
-            keys[area]["subAreaKey"] += 1
-            history[identifier] = {
-                "id": identifier,
+            history[areaName] = {
+                "id": areaName,
                 "history": []
             }
-        if(len(entry["pv"].split("*")) > 1):
-            # pv
-            pv = entry["pv"].split("*")[1]
-            pvs[area]["subArea"+str(keys[area]["subAreaKey"]-1)]["pvs"]["pv"+str(keys[identifier]["pvKey"])] = {
-                "name": pv,
+        elif(areaKey == "pvs"):
+            # area pvs
+            for pvKey, pvName in area[areaKey].items():
+                pvs[areaName]["pvs"][pvKey] = {
+                    "name": pvName,
+                    "enable": True,
+                    "latch": True,
+                    "notify": True,
+                    "lastAlarmVal": "",
+                    "lastAlarmTime": "",
+                    "lastAlarmAckTime": ""
+                }
+                history[areaName+"*"+pvName] = {
+                    "id": areaName+"*"+pvName,
+                    "history": []
+                }
+        elif("subArea" in areaKey):
+            # new subArea template
+            pvs[areaName][areaKey] = {
+                "name": areaValue["name"],
                 "enable": True,
-                "latch": True,
-                "notify": True,
-                "lastAlarmVal": "",
-                "lastAlarmTime": "",
-                "lastAlarmAckTime": ""
+                "pvs": {}
             }
-            keys[identifier]["pvKey"] += 1
-            history[identifier+"*"+pv] = {
-                "id": identifier+"*"+pv,
+            history[areaName+"="+areaValue["name"]] = {
+                "id": areaName+"="+areaValue["name"],
                 "history": []
             }
-
+            # subArea pvs
+            for pvKey, pvName in area[areaKey]["pvs"].items():
+                pvs[areaName][areaKey]["pvs"][pvKey] = {
+                    "name": pvName,
+                    "enable": True,
+                    "latch": True,
+                    "notify": True,
+                    "lastAlarmVal": "",
+                    "lastAlarmTime": "",
+                    "lastAlarmAckTime": ""
+                }
+                history[areaName+"="+areaValue["name"]+"*"+pvName] = {
+                    "id": areaName+"="+areaValue["name"]+"*"+pvName,
+                    "history": []
+                }
 
 for value in pvs.values():
     pvsFile.append(value)
 
 for value in history.values():
     historyFile.append(value)
-
 
 with open('./initDBData/pvs.json', 'w') as json_file:
     json.dump(pvsFile, json_file)

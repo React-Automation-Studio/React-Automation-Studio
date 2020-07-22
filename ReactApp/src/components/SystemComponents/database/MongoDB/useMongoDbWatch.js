1bd52837b1df0ad51357399a9792e2daeea7db2e
William Duckitt <william.duckitt@gmail.com>
2020-07-22 11:56:24 +0200

    formatting

--- ReactApp/src/components/SystemComponents/database/MongoDB/useMongoDbWatch.js
+++ ReactApp/src/components/SystemComponents/database/MongoDB/useMongoDbWatch.js
@@ -1,50 +1,49 @@
-import React, {useReducer,useContext,useState, useEffect } from 'react';
+import React, { useReducer, useContext, useState, useEffect } from 'react';
 import AutomationStudioContext from '../../AutomationStudioContext';
 
-const useMongoDbWatch= (dbURL) =>{
+const useMongoDbWatch = (dbURL) => {
 
-    const context=useContext(AutomationStudioContext);
-    const [dbWatchId,setDbWatchId]=useState(null);
-    const [data,setData]=useState(null);
-    const [writeAccess,setWriteAccess]=useState(false);
-    useEffect(()=>{
+    const context = useContext(AutomationStudioContext);
+    const [dbWatchId, setDbWatchId] = useState(null);
+    const [data, setData] = useState(null);
+    const [writeAccess, setWriteAccess] = useState(false);
+    useEffect(() => {
         const handleDatabaseReadWatchAndBroadcastAck = (msg) => {
             //console.log(msg)
             if (typeof msg !== 'undefined') {
                 setDbWatchId(msg.dbWatchId)
             }
         }
-        const handleNewDbLogReadWatchBroadcast=(msg) =>{
+        const handleNewDbLogReadWatchBroadcast = (msg) => {
             //console.log(msg)
             const newData = JSON.parse(msg.data);
             setData(newData);
-            setWriteAccess(msg.write_access) 
+            setWriteAccess(msg.write_access)
         }
-  
-    let socket = context.socket;
-    let jwt = JSON.parse(localStorage.getItem('jwt'));
-    if (jwt === null) {
-      jwt = 'unauthenticated'
-    }
-    if(dbURL){  
-    socket.emit('databaseReadWatchAndBroadcast', { 'dbURL':dbURL, 'clientAuthorisation': jwt },handleDatabaseReadWatchAndBroadcastAck)
-    socket.on('databaseWatchData:' + dbURL, handleNewDbLogReadWatchBroadcast);
-    }
 
-    return () => {
-        
-        if(dbURL){
-        if (dbWatchId !== null) {
-            socket.emit('remove_dbWatch', { dbURL: dbURL, dbWatchId: dbWatchId, 'clientAuthorisation': jwt });
+        let socket = context.socket;
+        let jwt = JSON.parse(localStorage.getItem('jwt'));
+        if (jwt === null) {
+            jwt = 'unauthenticated'
         }
-        socket.removeListener('databaseWatchData:' + dbURL, handleNewDbLogReadWatchBroadcast);
-    }
-    
-    }
-    
+        if (dbURL) {
+            socket.emit('databaseReadWatchAndBroadcast', { 'dbURL': dbURL, 'clientAuthorisation': jwt }, handleDatabaseReadWatchAndBroadcastAck)
+            socket.on('databaseWatchData:' + dbURL, handleNewDbLogReadWatchBroadcast);
+        }
+
+        return () => {
+
+            if (dbURL) {
+                if (dbWatchId !== null) {
+                    socket.emit('remove_dbWatch', { dbURL: dbURL, dbWatchId: dbWatchId, 'clientAuthorisation': jwt });
+                }
+                socket.removeListener('databaseWatchData:' + dbURL, handleNewDbLogReadWatchBroadcast);
+            }
+
+        }
+
+    }, [dbURL])
+
+    return ({ data: data, writeAccess: writeAccess, dbURl: dbURL })
 }
-    ,[dbURL])
-   
-    return({data:data,writeAccess:writeAccess,dbURl:dbURL})
-} 
 export default useMongoDbWatch

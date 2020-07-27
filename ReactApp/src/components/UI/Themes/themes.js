3105d813f21bb3ef205aad958d339054abaf60b2
William Duckitt <william.duckitt@gmail.com>
2020-07-27 17:41:04 +0200

    added navy theme

--- ReactApp/src/components/UI/Themes/themes.js
+++ ReactApp/src/components/UI/Themes/themes.js
@@ -2,12 +2,14 @@
 import defaultTheme from './defaultTheme'
 import lightTheme  from './lightTheme'
 import oceanTheme  from './oceanTheme'
+import navyTheme  from './navyTheme'
 import lightBlueGreyTheme from './lightBlueGreyTheme'
 const themes = { 
     "Dark": defaultTheme,
     "Light":lightTheme,
     "Ocean":oceanTheme,
-    "Light Blue-Grey":lightBlueGreyTheme
+    "Light Blue-Grey":lightBlueGreyTheme,
+    "Navy":navyTheme,
 
     }
     export default themes

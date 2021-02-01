a8d5833ca6509156f3b7a93fb7809c9aa84223e5
wduckitt <william.duckitt@gmail.com>
2021-02-01 17:33:58 +0200

    clean up file

--- ReactApp/src/LogIn.js
+++ ReactApp/src/LogIn.js
@@ -1,4 +1,4 @@
-import React, { useEffect,useState,useContext } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import AutomationStudioContext from './components/SystemComponents/AutomationStudioContext';
 import PropTypes from 'prop-types';
 import Avatar from '@material-ui/core/Avatar';
@@ -9,7 +9,6 @@ import InputLabel from '@material-ui/core/InputLabel';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
-
 import { Redirect } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import Dialog from '@material-ui/core/Dialog';
@@ -18,11 +17,10 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Slide from '@material-ui/core/Slide';
-
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
-const useStyles = makeStyles((theme)=> ({
+const useStyles = makeStyles((theme) => ({
   main: {
     width: 'auto',
     display: 'block', // Fix IE 11 issue.
@@ -53,68 +51,51 @@ const useStyles = makeStyles((theme)=> ({
     marginTop: theme.spacing(1) * 3,
   },
 }));
-
-const Login =(props)=>{
+const Login = (props) => {
   const classes = useStyles();
   const context = useContext(AutomationStudioContext);
-
-  const [emailAddress,setEmailAddress]=useState("")
-  const [password,setPassword]=useState("")
-  //const [authenticated,setAuthenticated]=useState(false)
-  const [authorised,setAuthorised]=useState(false)
-  const [authorisationFailed,setAuthorisationFailed]=useState(false)
-  const [authenticationFailed,setAuthenticationFailed]=useState(false)
-  const [submit,setSubmit]=useState(false);
-    
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [authorised, setAuthorised] = useState(false)
+  const [authorisationFailed, setAuthorisationFailed] = useState(false)
+  const [authenticationFailed, setAuthenticationFailed] = useState(false)
+  const [submit, setSubmit] = useState(false);
   
-  
-
-  
-  
-  useEffect(()=> {
-    if (submit==true){
-    
-    let port;
-    if (typeof process.env.REACT_APP_PyEpicsServerPORT === 'undefined') {
-      port = 5000;
-    }
-    else {
-      port = process.env.REACT_APP_PyEpicsServerPORT;
-    }
-
-    let pvServerBASEURL;
-    if (typeof process.env.REACT_APP_PyEpicsServerBASEURL === 'undefined') {
-      pvServerBASEURL = "http://127.0.0.1";
-    }
-    else {
-      pvServerBASEURL = process.env.REACT_APP_PyEpicsServerBASEURL;
-    }
-
-    let PyEpicsServerURL = pvServerBASEURL + ":" + port;
-
-    const requestOptions = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ user: { email: emailAddress, password: password } })
-    };
-    fetch(PyEpicsServerURL + '/api/login/local', requestOptions)
-      .then(response => response.json())
-      .then(msg => {
-         console.log(msg)
-   
+  useEffect(() => {
+    if (submit == true) {
+      let port;
+      if (typeof process.env.REACT_APP_PyEpicsServerPORT === 'undefined') {
+        port = 5000;
+      }
+      else {
+        port = process.env.REACT_APP_PyEpicsServerPORT;
+      }
+      let pvServerBASEURL;
+      if (typeof process.env.REACT_APP_PyEpicsServerBASEURL === 'undefined') {
+        pvServerBASEURL = "http://127.0.0.1";
+      }
+      else {
+        pvServerBASEURL = process.env.REACT_APP_PyEpicsServerBASEURL;
+      }
+      let PyEpicsServerURL = pvServerBASEURL + ":" + port;
+      const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ user: { username: username, password: password } })
+      };
+      fetch(PyEpicsServerURL + '/api/login/local', requestOptions)
+        .then(response => response.json())
+        .then(msg => {
+          console.log(msg)
           if (typeof msg.jwt !== 'undefined') {
             localStorage.setItem('jwt', JSON.stringify(msg.jwt));
           }
           else {
             localStorage.setItem('jwt', JSON.stringify(null));
           }
-          
           if (msg.login) {
             const jwt = JSON.parse(localStorage.getItem('jwt'));
-            
-      
-            
-            const {socket}=context;
+            const { socket } = context;
             if (socket.disconnected) {
               socket.open();
               socket.emit('AuthoriseClient', jwt);
@@ -122,172 +103,125 @@ const Login =(props)=>{
             else {
               socket.emit('AuthoriseClient', jwt);
             }
-            
-      
           }
-          setAuthenticationFailed(msg.login !== true );
-      
-        }        
-
-
-      )
+          setAuthenticationFailed(msg.login !== true);
+        }
+        )
       setSubmit(false)
     }
-    
-  },[submit]
-
+  }, [submit]
   )
-
-
-  
-
- 
-
-  
-
- 
-  
-  useEffect(()=> {
-    
-    
-    const handleAuthorisation=(msg)=>{
-    
+  useEffect(() => {
+    const handleAuthorisation = (msg) => {
       context.setUserData(msg.username, msg.roles);
-      setAuthenticationFailed(msg.successful !== true )
+      setAuthenticationFailed(msg.successful !== true)
       setAuthorised(msg.successful)
-      
-  
-  
     }
-    
-    const {socket} = context;
+    const { socket } = context;
     localStorage.removeItem('jwt');
-    
     socket.on('clientAuthorisation', handleAuthorisation);
-    return()=>{
-      
-    socket.removeListener('clientAuthorisation', handleAuthorisation);
+    return () => {
+      socket.removeListener('clientAuthorisation', handleAuthorisation);
     }
-  },[])
- 
-
-  
-    
-    
-    return (
-      <React.Fragment>
-
-        <Dialog
-          open={authorisationFailed}
-          TransitionComponent={Transition}
-          keepMounted
-
-          aria-labelledby="alert-Login-title1"
-          aria-describedby="alert-Login-slide-description1"
-        >
-          <DialogTitle id="alert-Login-title1">
-            Error!
+  }, [])
+  return (
+    <React.Fragment>
+      <Dialog
+        open={authorisationFailed}
+        TransitionComponent={Transition}
+        keepMounted
+        aria-labelledby="alert-Login-title1"
+        aria-describedby="alert-Login-slide-description1"
+      >
+        <DialogTitle id="alert-Login-title1">
+          Error!
         </DialogTitle>
-          <DialogContent>
-            <DialogContentText id="alert-Login-slide-description1">
-              Invalid username or password!
+        <DialogContent>
+          <DialogContentText id="alert-Login-slide-description1">
+            Invalid username or password!
           </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={()=>setAuthorisationFailed(false)} color="primary">
-              Ok
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setAuthorisationFailed(false)} color="primary">
+            Ok
           </Button>
-
-          </DialogActions>
-        </Dialog>
-        <Dialog
-          open={authenticationFailed}
-          TransitionComponent={Transition}
-          keepMounted
-
-          aria-labelledby="alert-Login-title2"
-          aria-describedby="alert-Login-slide-description2"
-        >
-          <DialogTitle id="alert-Login-title2">
-            {"Error!"}
-          </DialogTitle>
-          <DialogContent>
-            <DialogContentText id="alert-Login-slide-description2">
-              Authentication Failed!
+        </DialogActions>
+      </Dialog>
+      <Dialog
+        open={authenticationFailed}
+        TransitionComponent={Transition}
+        keepMounted
+        aria-labelledby="alert-Login-title2"
+        aria-describedby="alert-Login-slide-description2"
+      >
+        <DialogTitle id="alert-Login-title2">
+          {"Error!"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-Login-slide-description2">
+            Authentication Failed!
           </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={()=>setAuthenticationFailed(false)} color="primary">
-              Ok
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setAuthenticationFailed(false)} color="primary">
+            Ok
           </Button>
-
-          </DialogActions>
-        </Dialog>
-
-
-
-        <main className={classes.main}>
-
-          <Paper className={classes.paper}>
-            <Typography component="h1" variant="h3">
-              React
+        </DialogActions>
+      </Dialog>
+      <main className={classes.main}>
+        <Paper className={classes.paper}>
+          <Typography component="h1" variant="h3">
+            React
         </Typography>
-            <Typography component="h1" variant="h3">
-              Automation
+          <Typography component="h1" variant="h3">
+            Automation
           </Typography>
-            <Typography component="h1" variant="h3">
-              Studio
+          <Typography component="h1" variant="h3">
+            Studio
           </Typography>
-            <Avatar className={classes.avatar}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Sign in
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
           </Typography>
-            <form className={classes.form}>
-              <FormControl margin="normal" required fullWidth>
-                <InputLabel htmlFor="email">Username or Email Address</InputLabel>
-                <Input id="email" name="email" autoComplete="email" autoFocus onChange={(event)=>(setEmailAddress(event.target.value))}
-                 onKeyPress={(event)=>{
-                   if (event.key === 'Enter') {
-                      setEmailAddress(event.target.value)
-                      setSubmit(true)
-                     }
-                    }} 
-    />
-              </FormControl>
-              <FormControl margin="normal" required fullWidth>
-                <InputLabel htmlFor="password">Password</InputLabel>
-                <Input name="password" type="password" id="password" autoComplete="current-password" onChange={(event)=>(setPassword(event.target.value))} 
-                onKeyPress={(event)=>{
+          <form className={classes.form}>
+            <FormControl margin="normal" required fullWidth>
+              <InputLabel htmlFor="email">Username or Email Address</InputLabel>
+              <Input id="email" name="email" autoComplete="email" autoFocus onChange={(event) => (setUsername(event.target.value))}
+                onKeyPress={(event) => {
                   if (event.key === 'Enter') {
-                     setPassword(event.target.value)
-                     setSubmit(true)
-                    }
-                   }}
-                    />
-
-              </FormControl>
-          
-              <Button
-
-                fullWidth
-                variant="contained"
-                color="primary"
-                className={classes.submit}
-                onClick={()=> setSubmit(true)}
-              >
-                Sign in
+                    setUsername(event.target.value)
+                    setSubmit(true)
+                  }
+                }}
+              />
+            </FormControl>
+            <FormControl margin="normal" required fullWidth>
+              <InputLabel htmlFor="password">Password</InputLabel>
+              <Input name="password" type="password" id="password" autoComplete="current-password" onChange={(event) => (setPassword(event.target.value))}
+                onKeyPress={(event) => {
+                  if (event.key === 'Enter') {
+                    setPassword(event.target.value)
+                    setSubmit(true)
+                  }
+                }}
+              />
+            </FormControl>
+            <Button
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+              onClick={() => setSubmit(true)}
+            >
+              Sign in
             </Button>
-            </form>
-          </Paper>
-        </main>
-        {authorised && <Redirect to='/' />}
-      </React.Fragment>
-    );
-  }
-
-
-
-
+          </form>
+        </Paper>
+      </main>
+      {authorised && <Redirect to='/' />}
+    </React.Fragment>
+  );
+}
 export default Login;

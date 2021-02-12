0c7bb2a28331d566a2072b4650325d81d8fa6bf5
wduckitt <william.duckitt@gmail.com>
2021-02-12 17:27:00 +0200

    added check for no certificates

--- nginx/setupNginx.sh
+++ nginx/setupNginx.sh
@@ -7,18 +7,33 @@ if [ $SERVER_PORT == "443" ]
 then
     export listen="443 ssl"
     export styleguidelisten="6060 ssl"
-    if [ $HTTP_REDIRECT_TO_HTTPS == "true" ]
+    sed -i '10s/.*/    ssl_certificate server.cer;/' /etc/nginx/templates/default.conf.template
+    sed -i '11s/.*/    ssl_certificate_key server.key;/' /etc/nginx/templates/default.conf.template
+    sed -i '39s/.*/    ssl_certificate server.cer;/' /etc/nginx/templates/default.conf.template
+    sed -i '40s/.*/    ssl_certificate_key server.key;/' /etc/nginx/templates/default.conf.template
+    if [[ ! -z ${HTTP_REDIRECT_TO_HTTPS} ]]
     then
-        cat /custom/nginx.httpredirect.conf >> /etc/nginx/templates/default.conf.template
+        if [ $HTTP_REDIRECT_TO_HTTPS == "true" ]
+        then
+            cat /custom/nginx.httpredirect.conf >> /etc/nginx/templates/default.conf.template
+            cat /etc/nginx/templates/default.conf.template
+        fi
     fi
 else
-    if [ $SECURE == "true" ]
+    if [[ ! -z ${SECURE} ]]
     then
-        export listen="$SERVER_PORT ssl"
-        export styleguidelisten="6060 ssl"
+        if [ $SECURE == "true" ]
+        then
+            export listen="$SERVER_PORT ssl"
+            export styleguidelisten="6060 ssl"
+        else  
+            export listen="$SERVER_PORT"
+            export styleguidelisten="6060"  
+        fi
     else
         export listen="$SERVER_PORT"
         export styleguidelisten="6060"
+        
     fi
 fi
 echo listen = $listen

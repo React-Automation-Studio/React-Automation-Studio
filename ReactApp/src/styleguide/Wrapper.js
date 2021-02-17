358d01b763ced56372088821ebc3d221d297137b
William Duckitt <william.duckitt@gmail.com>
2021-02-17 12:24:05 +0200

    removed old url connection

--- ReactApp/src/styleguide/Wrapper.js
+++ ReactApp/src/styleguide/Wrapper.js
@@ -16,34 +16,34 @@ import { io } from 'socket.io-client';
 import   lightTheme  from '../components/UI/Themes/lightTheme'
 import ReactVisCssBaseline from '../components/SystemComponents/ReactVisCssBaseline';
 
-console.log('process.env', process.env)
-let port;
-if (typeof process.env.REACT_APP_PyEpicsServerStyleguidePORT === 'undefined') {
-  port = 5001;
-}
-else {
-  port = process.env.REACT_APP_PyEpicsServerStyleguidePORT;
-}
+// console.log('process.env', process.env)
+// let port;
+// if (typeof process.env.REACT_APP_PyEpicsServerStyleguidePORT === 'undefined') {
+//   port = 5001;
+// }
+// else {
+//   port = process.env.REACT_APP_PyEpicsServerStyleguidePORT;
+// }
 
-let pvServerBASEURL;
-if (typeof process.env.REACT_APP_PyEpicsServerBASEURL === 'undefined') {
-  pvServerBASEURL = "http://127.0.0.1";
-}
-else {
-  pvServerBASEURL = process.env.REACT_APP_PyEpicsServerBASEURL;
-}
+// let pvServerBASEURL;
+// if (typeof process.env.REACT_APP_PyEpicsServerBASEURL === 'undefined') {
+//   pvServerBASEURL = "http://127.0.0.1";
+// }
+// else {
+//   pvServerBASEURL = process.env.REACT_APP_PyEpicsServerBASEURL;
+// }
 
-let pvServerNamespace;
-if (typeof process.env.REACT_APP_PyEpicsServerNamespace === 'undefined') {
-  pvServerNamespace = "pvServer";
-}
-else {
-  pvServerNamespace = process.env.REACT_APP_PyEpicsServerNamespace;
-}
+// let pvServerNamespace;
+// if (typeof process.env.REACT_APP_PyEpicsServerNamespace === 'undefined') {
+//   pvServerNamespace = "pvServer";
+// }
+// else {
+//   pvServerNamespace = process.env.REACT_APP_PyEpicsServerNamespace;
+// }
 
-let PyEpicsServerURL = pvServerBASEURL + ":" + port + "/" + pvServerNamespace;
+// let PyEpicsServerURL = pvServerBASEURL + ":" + port + "/" + pvServerNamespace;
 
-console.log('PyEpicsServerURL: ', PyEpicsServerURL)
+// console.log('PyEpicsServerURL: ', PyEpicsServerURL)
 
 if (typeof window.socket === 'undefined') {
   window.socket = io("/pvServer", {

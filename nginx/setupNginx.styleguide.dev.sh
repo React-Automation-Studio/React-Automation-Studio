4a04fae4c901049cedaa53131a97ed74bfd2d234
William Duckitt <william.duckitt@gmail.com>
2021-02-17 12:31:01 +0200

    serving styleguide over nginx

---
+++ nginx/setupNginx.styleguide.dev.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "exporting dynamic enviroment variable"
+
+mkdir /etc/nginx/templates
+cp /custom/nginx.styleguide.dev.conf /etc/nginx/templates/default.conf.template
+if [ $SECURE == "true" ]
+then
+    export listen="3000 ssl"
+    export styleguidelisten="6060 ssl"
+    sed -i '8s/.*/    ssl_certificate server.cer;/' /etc/nginx/templates/default.conf.template
+    sed -i '9s/.*/    ssl_certificate_key server.key;/' /etc/nginx/templates/default.conf.template
+    sed -i '41s/.*/    ssl_certificate server.cer;/' /etc/nginx/templates/default.conf.template
+    sed -i '42s/.*/    ssl_certificate_key server.key;/' /etc/nginx/templates/default.conf.template
+else
+         export listen="3000"
+         export styleguidelisten="6060"
+fi
+
+echo listen = $listen
+
+exec "$@"

f907c83c820fd0e1566db4f48bbec45a15139db4
William Duckitt <william.duckitt@gmail.com>
2021-02-17 15:28:48 +0200

    changed certificates volume definition

--- nginx/setupNginx.sh
+++ nginx/setupNginx.sh
@@ -7,10 +7,10 @@ if [ $SERVER_PORT == "443" ]
 then
     export listen="443 ssl"
     export styleguidelisten="6060 ssl"
-    sed -i '10s/.*/    ssl_certificate server.cer;/' /etc/nginx/templates/default.conf.template
-    sed -i '11s/.*/    ssl_certificate_key server.key;/' /etc/nginx/templates/default.conf.template
-    sed -i '39s/.*/    ssl_certificate server.cer;/' /etc/nginx/templates/default.conf.template
-    sed -i '40s/.*/    ssl_certificate_key server.key;/' /etc/nginx/templates/default.conf.template
+    sed -i '10s/.*/    ssl_certificate certificates\/server.cer;/' /etc/nginx/templates/default.conf.template
+    sed -i '11s/.*/    ssl_certificate_key certificates\/server.key;/' /etc/nginx/templates/default.conf.template
+    sed -i '39s/.*/    ssl_certificate certificates\/server.cer;/' /etc/nginx/templates/default.conf.template
+    sed -i '40s/.*/    ssl_certificate_key certificates\/server.key;/' /etc/nginx/templates/default.conf.template
     if [[ ! -z ${HTTP_REDIRECT_TO_HTTPS} ]]
     then
         if [ $HTTP_REDIRECT_TO_HTTPS == "true" ]
@@ -26,10 +26,10 @@ else
         then
             export listen="$SERVER_PORT ssl"
             export styleguidelisten="6060 ssl"
-            sed -i '10s/.*/    ssl_certificate server.cer;/' /etc/nginx/templates/default.conf.template
-            sed -i '11s/.*/    ssl_certificate_key server.key;/' /etc/nginx/templates/default.conf.template
-            sed -i '39s/.*/    ssl_certificate server.cer;/' /etc/nginx/templates/default.conf.template
-            sed -i '40s/.*/    ssl_certificate_key server.key;/' /etc/nginx/templates/default.conf.template
+            sed -i '10s/.*/    ssl_certificate certificates\/server.cer;/' /etc/nginx/templates/default.conf.template
+            sed -i '11s/.*/    ssl_certificate_key certificates\/server.key;/' /etc/nginx/templates/default.conf.template
+            sed -i '39s/.*/    ssl_certificate certificates\/server.cer;/' /etc/nginx/templates/default.conf.template
+            sed -i '40s/.*/    ssl_certificate_key certificates\/server.key;/' /etc/nginx/templates/default.conf.template
         else  
             export listen="$SERVER_PORT"
             export styleguidelisten="6060"  

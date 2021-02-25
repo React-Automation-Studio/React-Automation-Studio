7f24354aeaeab14df31c26e06d4e65e9f37291e2
William Duckitt <william.duckitt@gmail.com>
2021-02-25 17:11:08 +0200

    changed to render

--- ReactApp/src/components/SystemComponents/ProtectedRoute.js
+++ ReactApp/src/components/SystemComponents/ProtectedRoute.js
@@ -2,20 +2,18 @@ import React, { useContext } from 'react';
 import AutomationStudioContext from './AutomationStudioContext';
 import BusyLoggingIn from './BusyLoggingIn';
 import { Route, Redirect } from 'react-router-dom'
+import LogIn from '../../LogIn';
 const ProtectedRoute=(props)=>{
     const Component=props.component;
     const context = useContext(AutomationStudioContext);
     const loggingIn=context.userData.loggingIn;
     let loggedIn = context.userData.loggedIn || process.env.REACT_APP_EnableLogin !== 'true';
     return(
-        <Route  path={props.path}  >
-
-          {     
-          ({ location }) => (
-          loggedIn?<Component location={location}/>:(loggingIn?<BusyLoggingIn/>:<Redirect to="/logIn" />)
-          )
-          }
-        </Route>
+     
+         <Route  path={props.path}  render={()=>( 
+          loggedIn?<Component />:(loggingIn?<BusyLoggingIn/>:<Redirect to="/logIn" />)
+          )}/>
+        
     )
 }
 export default ProtectedRoute;

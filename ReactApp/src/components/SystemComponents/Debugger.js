3a87dc46970da7f9746964a841b9cbb51692b5a9
William Duckitt <william.duckitt@gmail.com>
2021-03-02 15:39:48 +0200

    modified

--- ReactApp/src/components/SystemComponents/Debugger.js
+++ ReactApp/src/components/SystemComponents/Debugger.js
@@ -3,18 +3,8 @@ import React, {useContext,useEffect} from 'react';
 import AutomationStudioContext from './AutomationStudioContext';
 const Debugger=(props)=>{
     const context=useContext(AutomationStudioContext)
-    const newjwt = context.userTokens.accessToken;
-    useEffect(()=>{
-        const reconnect=()=>{
-         const jwt = context.userTokens.accessToken;
-         console.log("useEffect jwt",jwt)
-        }
-      reconnect();
-      return ()=>{
-       
-      }
-    },[newjwt])
-    console.log("newJwt",newjwt)
+    
+    console.log("loggedin:"+props.id,context.userData.loggedIn)
     return (<div/>)
 }
 export default Debugger;  

b3150e4c148773dc4f02e024c634b62ed2887413
wduckitt <william.duckitt@gmail.com>
2021-03-18 16:46:04 +0200

    addded component to display forbidden access

---
+++ ReactApp/src/components/SystemComponents/Forbidden.js
@@ -0,0 +1,32 @@
+import React, { useEffect,useState } from 'react';
+
+
+import Typography from '@material-ui/core/Typography';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { Redirect } from 'react-router-dom';
+
+
+
+const Forbidden = (props) => {
+
+const [time,setTime]=useState(3);
+useEffect(()=>{
+ 
+    const timer = setTimeout(()=>setTime(prev=>prev-1), 1000)
+    return()=>{
+        clearTimeout(timer)
+    }
+},[time])
+    return (
+        <div style={{ textAlign: 'center', paddingTop: '50vh' }}>
+            <Typography variant="h4">Access Forbidden</Typography>
+            <Typography variant="subtitle1">{"Redirecting in "+time+" seconds."}</Typography>
+            {time===0&&<Redirect to={"/"} />}
+            </div>
+            
+        
+    )
+
+}
+
+export default Forbidden;

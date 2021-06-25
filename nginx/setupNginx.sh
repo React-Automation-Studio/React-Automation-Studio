de19db93e4dcbf5fcb298d9db945668ab5e75626
wduckitt <william.duckitt@gmail.com>
2021-06-25 14:45:58 +0200

    fixed bash script if statement warning

--- nginx/setupNginx.sh
+++ nginx/setupNginx.sh
@@ -3,7 +3,7 @@ echo "exporting dynamic enviroment variable"
 
 mkdir -p /etc/nginx/templates
 cp /custom/nginx.production.conf /etc/nginx/templates/default.conf.template
-if [ $SERVER_PORT == "443" ]
+if [[ $SERVER_PORT == "443" ]]
 then
     export listen="443 ssl"
     export styleguidelisten="6060 ssl"
@@ -11,7 +11,7 @@ then
     sed -i 's/#insert ssl_certificate_key def here;/ssl_certificate_key certificates\/server.key;/g' /etc/nginx/templates/default.conf.template
     if [[ ! -z ${HTTP_REDIRECT_TO_HTTPS} ]]
     then
-        if [ $HTTP_REDIRECT_TO_HTTPS == "true" ]
+        if [[ $HTTP_REDIRECT_TO_HTTPS == "true" ]]
         then
             cat /custom/nginx.httpredirect.conf >> /etc/nginx/templates/default.conf.template
             cat /etc/nginx/templates/default.conf.template
@@ -20,7 +20,7 @@ then
 else
     if [[ ! -z ${SECURE} ]]
     then
-        if [ $SECURE == "true" ]
+        if [[ $SECURE == "true" ]]
         then
             export listen="$SERVER_PORT ssl"
             export styleguidelisten="6060 ssl"

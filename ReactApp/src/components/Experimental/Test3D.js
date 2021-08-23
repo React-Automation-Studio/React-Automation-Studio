01d8ae757a04676ea0e0e760e709ca5287caace6
wduckitt <wduckitt@tlabs.ac.za>
2021-08-23 16:09:26 +0200

    changed repsonsive control

--- ReactApp/src/components/Experimental/Test3D.js
+++ ReactApp/src/components/Experimental/Test3D.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react'
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 
@@ -14,12 +14,8 @@ import ThreeScene from '../Experimental/ThreeScene';
 
 import Card from '@material-ui/core/Card';
 import TraditionalLayout from '../UI/Layout/ComposedLayouts/TraditionalLayout.js';
-import {
 
-  makeVisFlexible,
 
-} from 'react-vis';
-const FlexibleThreeScene = makeVisFlexible(ThreeScene);
 const styles = theme => ({
   root: {
     flexGrow: 1,
@@ -31,12 +27,22 @@ const styles = theme => ({
     color: theme.palette.text.secondary,
   },
 });
-class Test3D extends React.Component {
-  
+const Test3D =(props)=> {
+  const ref = useRef(null);
+  const [width, setWidth] = useState(null);
+  const [height, setHeight] = useState(null);
+  useEffect(() => {
+    const handleResize = () => {
+      if (ref.current) {
+        setHeight(props.height ? props.height : ref.current.offsetHeight)
+        setWidth(props.width?props.width:ref.current.offsetWidth)
+      }
+    }
+    handleResize();
+    window.addEventListener("resize", handleResize)
+    return () => window.removeEventListener("resize", handleResize);
 
-  
-  render() {
-   
+  }, [ref, props.width, props.height, props.aspectRatio]);
     return (
       <div>
         <TraditionalLayout
@@ -56,9 +62,10 @@ class Test3D extends React.Component {
 
 
 
+            <div ref={ref} style={{ height: '100%', width: '100%' }}>
 
-              <FlexibleThreeScene />
-
+              <ThreeScene width={width} height={height} />
+              </div>
             </Grid>
 
             <Grid item xs={12} sm={12} md={12} lg={12} >
@@ -70,19 +77,19 @@ class Test3D extends React.Component {
                   alignItems="center"
                   spacing={2}
                 >  <Grid item xs={6}  >
-                    <TextInput pv='testIOC:Cube1:xRotation' macros={this.props['macros']} usePvMinMax={true} label="xRotation" step={0.01} prec={3} units={'rad'} />
+                    <TextInput pv='testIOC:Cube1:xRotation' macros={props['macros']} usePvMinMax={true} label="xRotation" step={0.01} prec={3} units={'rad'} />
 
                   </Grid>
                   <Grid item xs={6}  >
-                    <TextInput pv='testIOC:Cube1:yRotation' macros={this.props['macros']} usePvMinMax={true} label="yRotation" step={0.01} prec={3} units={'rad'} />
+                    <TextInput pv='testIOC:Cube1:yRotation' macros={props['macros']} usePvMinMax={true} label="yRotation" step={0.01} prec={3} units={'rad'} />
 
                   </Grid>
                   <Grid item xs={6} >
-                    <Slider pv='testIOC:Cube1:xRotation' macros={this.props['macros']} usePvMinMax={true} label="xRotation:" step={0.01} prec={3} />
+                    <Slider pv='testIOC:Cube1:xRotation' macros={props['macros']} usePvMinMax={true} label="xRotation:" step={0.01} prec={3} />
                   </Grid>
 
                   <Grid item xs={6} >
-                    <Slider pv='testIOC:Cube1:yRotation' macros={this.props['macros']} usePvMinMax={true} label="yRotation:" step={0.01} prec={3} />
+                    <Slider pv='testIOC:Cube1:yRotation' macros={props['macros']} usePvMinMax={true} label="yRotation:" step={0.01} prec={3} />
                   </Grid>
                 </Grid>
               </Card>
@@ -100,7 +107,7 @@ class Test3D extends React.Component {
 
     );
   }
-}
+
 
 Test3D.propTypes = {
   classes: PropTypes.object.isRequired,

37d240314c6491c570d171cb439327a91906c715
Ivo Hanak <hanak@ipp.cas.cz>
2022-05-18 12:13:35 +0200

    Cleared src/components/SystemComponents/database

--- ReactApp/src/components/SystemComponents/database/MongoDB/useMongoDbInsertOne.js
+++ ReactApp/src/components/SystemComponents/database/MongoDB/useMongoDbInsertOne.js
@@ -1,44 +1,38 @@
 import { useContext, useCallback,useRef,useEffect } from 'react';
 import AutomationStudioContext from '../../AutomationStudioContext';
-export const useMongoDbInsertOne = (props) => {
 
+export const useMongoDbInsertOne = (props) => {
     const context=useContext(AutomationStudioContext);
     const socket = context.socket;
     const jwt = context.userTokens.accessToken;
     const jwtRef = useRef(jwt);
     const socketRef = useRef(socket);
-    useEffect(() => {
-      if (jwt === null) {
-        jwtRef.current = 'unauthenticated'
-      }
-      else {
-        jwtRef.current = jwt;
-      }
-    }, [jwt])
-    useEffect(() => {
 
+    useEffect(() => {
+        if (jwt === null) {
+            jwtRef.current = 'unauthenticated'
+        }
+        else {
+            jwtRef.current = jwt;
+        }
+    }, [jwt])
+
+    useEffect(() => {
         socketRef.current = socket;
-      }, [socket])
+    }, [socket])
 
     const dbInsertOne = useCallback((props) => {
-
         if (props.dbURL && props.newEntry) {
-
-           
-            
             socketRef.current.emit('databaseInsertOne', { dbURL: props.dbURL, 'newEntry': props.newEntry, 'clientAuthorisation': jwtRef.current }, (data) => {
-                //console.log("ackdata", data);
                 if (data !== "OK") {
-
                     console.log("InsertOne  unsuccessful")
                 }
             });
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
-    return (dbInsertOne);
-   
-}
 
+    return (dbInsertOne);
+}
 
 export default useMongoDbInsertOne
--- ReactApp/src/components/SystemComponents/database/MongoDB/useMongoDbUpdateOne.js
+++ ReactApp/src/components/SystemComponents/database/MongoDB/useMongoDbUpdateOne.js
@@ -1,41 +1,39 @@
 
 import { useContext, useCallback,useRef,useEffect } from 'react';
 import AutomationStudioContext from '../../AutomationStudioContext';
+
 const useMongoDbUpdateOne = (props) => {
     const context=useContext(AutomationStudioContext);
     const socket = context.socket;
     const jwt = context.userTokens.accessToken;
     const jwtRef = useRef(jwt);
     const socketRef = useRef(socket);
+
     useEffect(() => {
         if (jwt === null) {
-          jwtRef.current = 'unauthenticated'
+            jwtRef.current = 'unauthenticated'
         }
         else {
-          jwtRef.current = jwt;
+            jwtRef.current = jwt;
         }
-      }, [jwt])
-      useEffect(() => {
-  
-          socketRef.current = socket;
-        }, [socket])
+    }, [jwt])
+
+    useEffect(() => {
+        socketRef.current = socket;
+    }, [socket])
 
     const dbUpdateOne = useCallback((props) => {
-
         if (props.dbURL && props.update&&props.id) {
-
-           
             socketRef.current.emit('databaseUpdateOne', { dbURL: props.dbURL, 'id': props.id, 'newvalues': props.update, 'clientAuthorisation': jwtRef.current }, (data) => {
-  
-            if (data !== "OK") {
-
-                console.log("updateOne  unsuccessful")
-            }
-        });
+                if (data !== "OK") {
+                    console.log("updateOne  unsuccessful")
+                }
+            });
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
+
     return (dbUpdateOne);
-   
 }
+
 export default useMongoDbUpdateOne

24717f5fec3dc31fafb2b4e6f976a2db50e02c68
William Duckitt <wdduckitt@sun.ac.za>
2022-08-18 14:51:41 +0200

    removed unnecessary imports

--- ReactApp/src/styleguide/Wrapper.js
+++ ReactApp/src/styleguide/Wrapper.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, BrowserRouter, Switch } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "typeface-roboto";
 import {
   ThemeProvider,

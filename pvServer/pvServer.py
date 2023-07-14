99307e326ff7768f9822a72197b92323798cb54d
William Duckitt <wdduckitt@sun.ac.za>
2023-07-14 12:19:39 +0200

    updated the version number

--- pvServer/pvServer.py
+++ pvServer/pvServer.py
@@ -68,7 +68,7 @@ REACT_DISABLE_STANDARD_LOGIN = os.getenv("REACT_APP_DisableStandardLogin", None)
 
 log.info("")
 log.info("**************************************")
-log.info("React Automation Studio V4.0.0")
+log.info("React Automation Studio V4.0.2")
 log.info("")
 log.info("pvServer Environment Variables:")
 log.info("")

4eb61ef6822fe600083bb6509feee5f6ad40c3a9
William Duckitt <wdduckitt@sun.ac.za>
2023-09-12 17:33:09 +0200

    adding storybook

---
+++ ReactApp/src/components/BaseComponents/TextInput.stories.js
@@ -0,0 +1,24 @@
+import  TextInput  from './TextInput';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Example/TextInput',
+  component: TextInput,
+  parameters: {
+   
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ['autodocs'],
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+  
+  },
+};
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Primary = {
+  args: {
+    
+  },
+};
+

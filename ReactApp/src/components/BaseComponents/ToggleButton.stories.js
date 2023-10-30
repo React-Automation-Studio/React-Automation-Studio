01e79d0cf66c9c874a9c61e9995e6dbdc0522701
William Duckitt <wdduckitt@sun.ac.za>
2023-10-30 14:54:36 +0200

    added examples

---
+++ ReactApp/src/components/BaseComponents/ToggleButton.stories.js
@@ -0,0 +1,32 @@
+import  ToggleButton  from './ToggleButton';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Example/ToggleButton',
+  component: ToggleButton,
+  parameters: {
+   
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ['autodocs'],
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+  
+  },
+};
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Primary = {
+  args: {
+    pv:'testIOC:BO1',
+    label:'testIOC:BO1 ',    
+  },
+};
+
+export const Momentary = {
+  args: {
+    pv:'testIOC:BO1',
+    label:'testIOC:BO1 ',
+    momentary:true,    
+  },
+};

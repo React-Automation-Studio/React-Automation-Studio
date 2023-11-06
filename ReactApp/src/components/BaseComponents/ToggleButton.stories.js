6c5636f639b4cd75231293ecc5cff2542316a958
William Duckitt <wdduckitt@sun.ac.za>
2023-11-06 16:36:48 +0200

    added momentary

--- ReactApp/src/components/BaseComponents/ToggleButton.stories.js
+++ ReactApp/src/components/BaseComponents/ToggleButton.stories.js
@@ -2,7 +2,7 @@ import  ToggleButton  from './ToggleButton';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Example/ToggleButton',
+ 
   component: ToggleButton,
   parameters: {
    

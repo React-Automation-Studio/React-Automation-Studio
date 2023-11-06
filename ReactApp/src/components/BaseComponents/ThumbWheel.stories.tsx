f17b81b70321d186845bb8177279eacc066f35bb
William Duckitt <wdduckitt@sun.ac.za>
2023-11-06 15:30:57 +0200

    changed to typescript

---
+++ ReactApp/src/components/BaseComponents/ThumbWheel.stories.tsx
@@ -0,0 +1,54 @@
+import ThumbWheel from "./ThumbWheel";
+import TextOutput from "./TextOutput";
+import type { Meta, StoryObj } from "@storybook/react";
+
+export default {
+  component: ThumbWheel,
+  parameters: {},
+
+  tags: ["autodocs"],
+
+  argTypes: {},
+} as Meta;
+
+const Template: StoryObj = {
+  render: ({ ...args }) => {
+    console.log(args);
+    return (
+      <div>
+        <TextOutput
+          {...args}
+          usePvLabel={true}
+          usePvUnits={true}
+          usePvMinMax={true}
+          alarmSensitive={true}
+        />
+        <br />
+        <ThumbWheel {...args} />
+      </div>
+    );
+  },
+};
+
+export const Overview = {
+  ...Template,
+  args: {
+    pv: "$(device):test$(id)",
+    macros: { "$(device)": "testIOC", "$(id)": "2" },
+    prec_integer: 4,
+    prec_decimal: 3,
+    prec: 3,
+    usePvMinMax: true,
+  },
+};
+
+export const Primary = {
+  args: {
+    pv: "$(device):test$(id)",
+    macros: { "$(device)": "testIOC", "$(id)": "2" },
+    prec_integer: 4,
+    prec_decimal: 3,
+    prec: 3,
+    usePvMinMax: true,
+  },
+};

2a7f246a653d49c856af885b6b8bdaf4b10132e8
William Duckitt <wdduckitt@sun.ac.za>
2023-11-16 14:41:21 +0200

    added fullpath prop

--- ReactApp/.storybook/ImportPath.tsx
+++ ReactApp/.storybook/ImportPath.tsx
@@ -7,10 +7,10 @@ export const ImportPath = (props): JSX.Element => {
   const arr = context?.primaryStory?.kind?.split("/");
 
   const componentName = arr[arr.length - 1];
-
+ 
   const path = `import  ${componentName}  from '${props.libName}/${context?.primaryStory?.kind}';`;
 
-  return <Source language="js" code={path} />;
+  return <Source language="js" code={props.fullPath?props.fullPath:path} />;
 };
 
 export default ImportPath;

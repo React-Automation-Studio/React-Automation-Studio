d0c3230d2bc9d7ce9b692242c05993e7c0ef3efd
William Duckitt <wdduckitt@sun.ac.za>
2023-11-20 14:35:40 +0200

    added alarmhandler stories

---
+++ ReactApp/src/components/AlarmHandler/AlarmHandler.stories.jsx
@@ -0,0 +1,25 @@
+import  AlarmHandler  from './AlarmHandler';
+
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  component: AlarmHandler,
+  title:"AlarmHandler/Component Not Loading",
+  parameters: {
+  
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ['autodocs'],
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+  
+  },
+};
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Primary = {
+  args: {
+   
+    
+  },
+};

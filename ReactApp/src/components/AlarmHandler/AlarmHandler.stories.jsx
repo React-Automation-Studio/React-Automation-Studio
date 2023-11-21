ad2e18ad70fed5d56472848f3697d333f4cd9462
William Duckitt <wdduckitt@sun.ac.za>
2023-11-21 16:04:52 +0200

    added story

--- ReactApp/src/components/AlarmHandler/AlarmHandler.stories.jsx
+++ ReactApp/src/components/AlarmHandler/AlarmHandler.stories.jsx
@@ -4,12 +4,12 @@ import  AlarmHandler  from './AlarmHandler';
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   component: AlarmHandler,
-  title:"AlarmHandler/Component Not Loading",
+  title:"AlarmHandler/AlarmHandler Component",
   parameters: {
   
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
-  tags: ['autodocs'],
+  tags: ["autodocs"],
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
   
@@ -19,6 +19,13 @@ export default {
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Primary = {
   args: {
+    titleProps:{
+      title: "Demo Alarm Handler",
+      alignTitle: "center",
+      titleVariant: "h6",
+      titleTextStyle: { textTransform: 'uppercase' }
+  },
+  alarmDatabaseName:"demoAlarmDatabase",
    
     
   },

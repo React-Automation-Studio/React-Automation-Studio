b3a126de46425793db192a95d32be56f16e33b6d
wduckitt <william.duckitt@gmail.com>
2023-11-30 16:01:12 +0200

    removed console log and comment

--- ReactApp/src/App.jsx
+++ ReactApp/src/App.jsx
@@ -1,16 +1,13 @@
-
-import React from 'react';
-import RasAppCore from './components/SystemComponents/RasAppCore';
-import themes  from './components/UI/Themes/themes';
-import Routes from './Routes';
-console.log(themes)
+import React from "react";
+import RasAppCore from "./components/SystemComponents/RasAppCore";
+import themes from "./components/UI/Themes/themes";
+import Routes from "./Routes";
 const App = (props) => {
   return (
-    <RasAppCore  themes={themes} defaultTheme={'Ocean'}   >
+    <RasAppCore themes={themes} defaultTheme={"Ocean"}>
       <Routes />
     </RasAppCore>
-    // <div>hello</div>
   );
-}
+};
 
 export default App;

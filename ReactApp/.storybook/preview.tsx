d452e752414bd2d4a5285c8e1223c043c7798bda
wduckitt <william.duckitt@gmail.com>
2023-11-30 15:56:07 +0200

    remove comment code

--- ReactApp/.storybook/preview.tsx
+++ ReactApp/.storybook/preview.tsx
@@ -42,19 +42,4 @@ const preview: Preview = {
 
 export default preview;
 
-// import React from 'react';
 
-// import { Preview } from '@storybook/react';
-// import Wrapper from '../src/styleguide/Wrapper'
-// const preview: Preview = {
-//   decorators: [
-//     (Story) => (
-//      <Wrapper>
-//         {/* 👇 Decorators in Storybook also accept a function. Replace <Story/> with Story() to enable it  */}
-//         <Story />
-//       </Wrapper>
-//     ),
-//   ],
-// };
-
-// export default preview;

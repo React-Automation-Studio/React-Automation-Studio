9ffb18c7e3fea07d2a80e5261bfc7a81705f487f
William Duckitt <wdduckitt@sun.ac.za>
2023-12-07 16:14:08 +0200

    added plugins props

--- ReactApp/src/components/Examples/AreaDetectorSimExample.jsx
+++ ReactApp/src/components/Examples/AreaDetectorSimExample.jsx
@@ -1,18 +1,51 @@
-import React from 'react';
-import AreaDetector from '../AreaDetector/AreaDetector'
+import React from "react";
+import AreaDetector from "../AreaDetector/AreaDetector";
 
 const AreaDetectorSimExample = () => {
-    return (
-        <AreaDetector
-            titleProps={{
-                title: "Area Detector Sim Example",
-                alignTitle: "center",
-                titleVariant: "h6",
-                titleTextStyle: { textTransform: 'uppercase' }
-            }}
-            macros={{"$(P)": "ras:adsim:", "$(R)": "cam1:"}}
-        />
-    );
+  return (
+    <AreaDetector
+      titleProps={{
+        title: "Area Detector Sim Example",
+        alignTitle: "center",
+        titleVariant: "h6",
+        titleTextStyle: { textTransform: "uppercase" },
+      }}
+      macros={{ "$(P)": "ras:adsim:", "$(R)": "cam1:" }}
+      pluginTypes={[
+        "image1",
+        "Pva1",
+        "Proc1",
+        "Trans1",
+        "CC1",
+        "CC2",
+        "Over1",
+        "ROI1",
+        "ROI2",
+        "ROI3",
+        "ROI4",
+        "Stats1",
+        "Stats2",
+        "Stats3",
+        "Stats4",
+        "Stats5",
+        "Scatter1",
+        "Gather1",
+        "ROIStat1",
+        "CB1",
+        "Attr1",
+        "FFT1",
+        "Codec1",
+        "Codec2",
+        "BadPix1",
+        "netCDF1",
+        "TIFF1",
+        "JPEG1",
+        "Nexus1",
+        "Magick1",
+        "HDF1",
+      ]}
+    />
+  );
 };
 
 export default AreaDetectorSimExample;

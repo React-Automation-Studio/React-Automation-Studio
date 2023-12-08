12e666c6d773a2edc1e252917da6e97f506f14c1
William Duckitt <wdduckitt@sun.ac.za>
2023-12-08 15:12:55 +0200

    added quick launch buttons

--- ReactApp/src/components/AreaDetector/Info.jsx
+++ ReactApp/src/components/AreaDetector/Info.jsx
@@ -9,13 +9,12 @@ import TableRow from "@mui/material/TableRow";
 
 import TableCell from "@mui/material/TableCell";
 import SelectionInput from "../BaseComponents/SelectionInput";
-import ActionButton from "../BaseComponents/ActionButton";
+import MultiActionButton from "./MultiActionButton";
 
-import Stack from "@mui/system/Stack";
 import { useTheme } from "@mui/system";
 import { StyledTableCell } from "./TableStyles";
 import { Typography } from "@mui/material";
-
+import Stack from "@mui/system/Stack";
 const Info = (props) => {
   const muiTextFieldProps = { size: "small", variant: "standard" };
   const theme = useTheme();
@@ -27,6 +26,149 @@ const Info = (props) => {
       <br/>
       <br/>
       Set the acquire period two 2 seconds in the Collect accordian.
+
+      <Stack direction="row" spacing={2}>
+        <MultiActionButton macros={props.macros}  actionString="Peaks"
+        simMode={"Peaks"}
+        gainX={1}
+        gainY={1}
+        gain={200}
+        gainRed={1}
+        gainGreen={2}
+        gainBlue={3}
+        Offset={0}
+        Noise={0}
+        peakStartX={0}
+        peakStartY={0}
+        peakNumX={20}
+        peakNumY={20}
+        peakStepX={128}
+        peakStepY={256}
+        peakWidthX={10}
+        peakWidthY={20}
+        peakVariation={0}
+        xSine1Amplitude={10}
+        xSine1Frequency={200}
+        xSine1Phase={0}
+        xSine2Amplitude={0}
+        xSine2Frequency={50}
+        xSine2Phase={0}
+        ySine1Amplitude={5}
+        ySine1Frequency={100}
+        ySine1Phase={0}
+        ySine2Amplitude={0}
+        ySine2Frequency={10}
+        ySine2Phase={0}
+        xSineOperation={"Add"}
+        ySineOperation={"Add"}
+
+        />
+        <MultiActionButton macros={props.macros}  actionString="Sine"
+        simMode={"Sine"}
+        gainX={1}
+        gainY={1}
+        gain={200}
+        gainRed={1}
+        gainGreen={2}
+        gainBlue={3}
+        Offset={0}
+        Noise={0}
+        peakStartX={0}
+        peakStartY={0}
+        peakNumX={20}
+        peakNumY={20}
+        peakStepX={128}
+        peakStepY={256}
+        peakWidthX={10}
+        peakWidthY={20}
+        peakVariation={0}
+        xSine1Amplitude={10}
+        xSine1Frequency={200}
+        xSine1Phase={0}
+        xSine2Amplitude={0}
+        xSine2Frequency={50}
+        xSine2Phase={0}
+        ySine1Amplitude={5}
+        ySine1Frequency={100}
+        ySine1Phase={0}
+        ySine2Amplitude={0}
+        ySine2Frequency={10}
+        ySine2Phase={0}
+        xSineOperation={"Add"}
+        ySineOperation={"Add"}
+
+        />
+        <MultiActionButton macros={props.macros}  actionString="Linear Ramp"
+        simMode={"LinearRamp"}
+        gainX={1}
+        gainY={1}
+        gain={200}
+        gainRed={1}
+        gainGreen={2}
+        gainBlue={3}
+        Offset={0}
+        Noise={0}
+        peakStartX={0}
+        peakStartY={0}
+        peakNumX={20}
+        peakNumY={20}
+        peakStepX={128}
+        peakStepY={256}
+        peakWidthX={10}
+        peakWidthY={20}
+        peakVariation={0}
+        xSine1Amplitude={10}
+        xSine1Frequency={200}
+        xSine1Phase={0}
+        xSine2Amplitude={0}
+        xSine2Frequency={50}
+        xSine2Phase={0}
+        ySine1Amplitude={5}
+        ySine1Frequency={100}
+        ySine1Phase={0}
+        ySine2Amplitude={0}
+        ySine2Frequency={10}
+        ySine2Phase={0}
+        xSineOperation={"Add"}
+        ySineOperation={"Add"}
+
+        />
+         <MultiActionButton macros={props.macros}  actionString="Offset&Noise"
+        simMode={"Offset&Noise"}
+        gainX={1}
+        gainY={1}
+        gain={200}
+        gainRed={1}
+        gainGreen={2}
+        gainBlue={3}
+        Offset={0}
+        Noise={0}
+        peakStartX={0}
+        peakStartY={0}
+        peakNumX={20}
+        peakNumY={20}
+        peakStepX={128}
+        peakStepY={256}
+        peakWidthX={10}
+        peakWidthY={20}
+        peakVariation={0}
+        xSine1Amplitude={10}
+        xSine1Frequency={200}
+        xSine1Phase={0}
+        xSine2Amplitude={0}
+        xSine2Frequency={50}
+        xSine2Phase={0}
+        ySine1Amplitude={5}
+        ySine1Frequency={100}
+        ySine1Phase={0}
+        ySine2Amplitude={0}
+        ySine2Frequency={10}
+        ySine2Phase={0}
+        xSineOperation={"Add"}
+        ySineOperation={"Add"}
+
+        />
+        </Stack>
       
       </Typography>
    

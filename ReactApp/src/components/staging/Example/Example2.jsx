1ef1e2e46feb655bdaed1c5eb5775d6b17176dcc
William Duckitt <wdduckitt@sun.ac.za>
2024-01-22 18:22:38 +0200

    Refactor Example2 component structure and styling

--- ReactApp/src/components/staging/Example/Example2.jsx
+++ ReactApp/src/components/staging/Example/Example2.jsx
@@ -1,109 +1,99 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import withStyles from '@mui/styles/withStyles';
-
-import TextInput from '../../../components/BaseComponents/TextInput';
-import TextOutput from '../../../components/BaseComponents/TextOutput';
-
-import Slider from '../../BaseComponents/Slider';
-import GraphY from '../../../components/BaseComponents/GraphY';
-
-import Grid from '@mui/material/Grid';
-
-import SideBar from '../../../components/SystemComponents/SideBar';
-
-import { lime } from '@mui/material/colors';
-const styles = theme => ({
-  root: {
-    padding: 0,
-    spacing: 0,
-    direction: 'row',
-    alignItems: 'stretch',
-    justify: "flex-start",
-    overflowX: "hidden",
-    overflowY: "hidden",
-  },
-});
-
-class Example2 extends React.Component {
-  render() {
-    return (
-      <React.Fragment>
-        <SideBar/>
-        <div >
+import React from "react";
+import TextInput from "../../../components/BaseComponents/TextInput";
+import TextOutput from "../../../components/BaseComponents/TextOutput";
+import Slider from "../../BaseComponents/Slider";
+import GraphY from "../../../components/BaseComponents/GraphY";
+import Grid from "@mui/material/Grid";
+import SideBar from "../../../components/SystemComponents/SideBar";
+import { lime } from "@mui/material/colors";
+import {useTheme} from "@mui/material/styles";
+const Example2 = () => {
+  const theme = useTheme();
+  return (
+    <React.Fragment>
+      <SideBar />
+      <div>
         <Grid
-           style={{padding:8}}
-           container item
-           direction="row"
-           justifyContent="center"
-           spacing={1}
-           alignItems="stretch"
+          style={{ padding: 8 }}
+          container
+          item
+          direction="row"
+          justifyContent="center"
+          spacing={1}
+          alignItems="stretch"
         >
-          <Grid item xs={6}  >
+          <Grid item xs={6}>
             <TextInput
-                pv='$(device):amplitude'
-                macros={{'$(device)':'testIOC'}}
-                usePvLabel={true}
-                
-                prec={3}
-                alarmSensitive={true}
+              pv="$(device):amplitude"
+              macros={{ "$(device)": "testIOC" }}
+              usePvLabel={true}
+              prec={3}
+              alarmSensitive={true}
             />
           </Grid>
-          <Grid item  xs={6}>
+          <Grid item xs={6}>
             <TextOutput
-              pv='$(device):test3'
-              macros={{'$(device)':'testIOC'}}
-              usePvLabel={true} 
-              prec={3} alarmSensitive={true}
+              pv="$(device):test3"
+              macros={{ "$(device)": "testIOC" }}
+              usePvLabel={true}
+              prec={3}
+              alarmSensitive={true}
             />
           </Grid>
-          <Grid item xs={3}  >
+          <Grid item xs={3}>
             <TextInput
-                pv='$(device):amplitude'
-                macros={{'$(device)':'testIOC'}}
-                usePvLabel={true}
-                
-                prec={3}
-                alarmSensitive={true}
+              pv="$(device):amplitude"
+              macros={{ "$(device)": "testIOC" }}
+              usePvLabel={true}
+              prec={3}
+              alarmSensitive={true}
             />
           </Grid>
-          <Grid item  xs={3}>
+          <Grid item xs={3}>
             <TextOutput
-              pv='$(device):test3'
-              macros={{'$(device)':'testIOC'}}
-              usePvLabel={true} 
-              prec={3} alarmSensitive={true}
+              pv="$(device):test3"
+              macros={{ "$(device)": "testIOC" }}
+              usePvLabel={true}
+              prec={3}
+              alarmSensitive={true}
             />
           </Grid>
 
-          <Grid item xs={3}  >
+          <Grid item xs={3}>
             <TextInput
-                pv='$(device):amplitude'
-                macros={{'$(device)':'testIOC'}}
-                usePvLabel={true}
-                
-                prec={3}
-                alarmSensitive={true}
+              pv="$(device):amplitude"
+              macros={{ "$(device)": "testIOC" }}
+              usePvLabel={true}
+              prec={3}
+              alarmSensitive={true}
             />
           </Grid>
-          <Grid item  xs={3}>
+          <Grid item xs={3}>
             <TextOutput
-              pv='$(device):test3'
-              macros={{'$(device)':'testIOC'}}
-              usePvLabel={true} 
-              prec={3} alarmSensitive={true}
+              pv="$(device):test3"
+              macros={{ "$(device)": "testIOC" }}
+              usePvLabel={true}
+              prec={3}
+              alarmSensitive={true}
             />
           </Grid>
 
-          <Grid item xs={12} >
-            <div style={{ height: '50vh', width:'96vw',}}>
-              <GraphY  pvs={['testIOC:test4','testIOC:test5'] } legend={['Sine Wave','Amplitude']} lineColor={[this.props.theme.palette.secondary.main,lime['400']]}/>
+          <Grid item xs={12}>
+            <div style={{ height: "50vh", width: "96vw" }}>
+              <GraphY
+                pvs={["testIOC:test4", "testIOC:test5"]}
+                legend={["Sine Wave", "Amplitude"]}
+                lineColor={[
+                  theme.palette.secondary.main,
+                  lime["400"],
+                ]}
+              />
             </div>
           </Grid>
-          <Grid item xs={12}  >
+          <Grid item xs={12}>
             <Slider
-              pv='$(device):amplitude'
-              macros={{'$(device)':'testIOC'}}
+              pv="$(device):amplitude"
+              macros={{ "$(device)": "testIOC" }}
               usePvMinMax={true}
               min={1000}
               max={500}
@@ -112,13 +102,8 @@ class Example2 extends React.Component {
           </Grid>
         </Grid>
       </div>
-      </React.Fragment>
-    );
-  }
-}
-
-Example2.propTypes = {
-  classes: PropTypes.object.isRequired,
+    </React.Fragment>
+  );
 };
 
-export default withStyles(styles,{withTheme:true})(Example2);
+export default Example2;

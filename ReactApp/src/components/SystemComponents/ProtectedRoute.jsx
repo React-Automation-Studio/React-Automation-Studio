d3b4ac0c33bc4d4001ea79aabb942a76e4dc40ad
William Duckitt <wdduckitt@sun.ac.za>
2024-01-22 16:37:06 +0200

    Refactor ProtectedRoute component and update react-router-dom

--- ReactApp/src/components/SystemComponents/ProtectedRoute.jsx
+++ ReactApp/src/components/SystemComponents/ProtectedRoute.jsx
@@ -1,53 +1,47 @@
-import React, { useContext } from 'react';
-import AutomationStudioContext from './AutomationStudioContext';
-import BusyLoggingIn from './BusyLoggingIn';
-import Forbidden from './Forbidden';
-import { Route, Redirect } from 'react-router-dom'
+import React, { useContext } from "react";
+import AutomationStudioContext from "./AutomationStudioContext";
+import BusyLoggingIn from "./BusyLoggingIn";
+import Forbidden from "./Forbidden";
+import { useLocation, Navigate } from "react-router-dom";
 
 const ProtectedRoute = (props) => {
+  const location = useLocation();
   const Component = props.component;
   const context = useContext(AutomationStudioContext);
   const loggingIn = context.userData.loggingIn;
-  const loggedIn = context.userData.loggedIn || import.meta.env.VITE_EnableLogin !== 'true';
-  const userRoles= context.userData.roles;
-  const {roles}=props;
+  const loggedIn =
+    context.userData.loggedIn || import.meta.env.VITE_EnableLogin !== "true";
+  const userRoles = context.userData.roles;
+  const { roles } = props;
   let forbidden;
 
-  if (roles){    
-    if (roles.length>0){
-      let found=userRoles.some(r=> roles.includes(r));
-      forbidden=!found;
+  if (roles) {
+    if (roles.length > 0) {
+      let found = userRoles.some((r) => roles.includes(r));
+      forbidden = !found;
+    } else {
+      forbidden = false;
     }
-    else{
-      forbidden=false;
-    }
+  } else {
+    forbidden = false;
   }
-  else{
-    forbidden=false;
-  }
- 
-  return (
-    <Route path={props.path} exact={props.exact}  >
-      {
-        (routeProps) => (
-          loggedIn ?
-            forbidden?
-            <Forbidden/>
-            :<Component {...routeProps} /> :
-            (loggingIn ?
-              <BusyLoggingIn />
-              :
-              <Redirect
-                to={{
-                  pathname: "/login",
-                  state: { from: routeProps.location }
-                }}
-              />
-            )
-        )
-      }
-    </Route>
-  )
-}
+
+  return loggedIn ? (
+    forbidden ? (
+      <Forbidden />
+    ) : (
+      props.children
+    )
+  ) : loggingIn ? (
+    <BusyLoggingIn />
+  ) : (
+    <Navigate
+      to={{
+        pathname: "/login",
+        state: { from: location },
+      }}
+    />
+  );
+};
 
 export default ProtectedRoute;

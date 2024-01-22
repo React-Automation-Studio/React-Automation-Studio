c479e5af5619db229f022b906a0ed4951901c69d
William Duckitt <wdduckitt@sun.ac.za>
2024-01-22 17:55:33 +0200

    Refactor Staging component layout and styling

--- ReactApp/src/components/staging/Staging.jsx
+++ ReactApp/src/components/staging/Staging.jsx
@@ -1,65 +1,48 @@
-import React, { Component } from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import Button from "@mui/material/Button";
+import TraditionalLayout from "../../components/UI/Layout/ComposedLayouts/TraditionalLayout";
+import Grid from "@mui/material/Grid";
 
-import withStyles from '@mui/styles/withStyles';
-import { withRouter } from 'react-router-dom';
-
-import { Link } from 'react-router-dom'
-import Button from '@mui/material/Button';
-import TraditionalLayout from '../../components/UI/Layout/ComposedLayouts/TraditionalLayout';
-import Grid from '@mui/material/Grid';
-
-const styles = theme => ({
-  root: {
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.default,
-  },
-  center: {
-    margin: 'auto',
-
-    width: '15%',
-    height: '50%'
-  },
-  button: {
-    marginTop: '10px',
-    paddingTop: '10px'
-  }
-});
-
-class Staging extends Component {
-  constructor(props) {
-    super(props);
-    this.logout = this.logout.bind(this);
-  }
-
-  logout() {
-    localStorage.removeItem('jwt');
-  }
-
-  render() {
-    const { classes } = this.props;
-
-    return (
-      <TraditionalLayout
-        title="Staging Area"
-        denseAppBar
-        alignTitle="center"
+const Staging = () => {
+  return (
+    <TraditionalLayout title="Staging Area" denseAppBar alignTitle="center">
+      <Grid
+        container
+        direction="row"
+        item
+        justifyContent="center"
+        spacing={1}
+        alignItems="center"
+        style={{ paddingTop: 64 }}
       >
-        <Grid container direction="row" item justifyContent="center" spacing={1} alignItems="center" style={{ paddingTop: 64 }}>
-          <Grid item lg={4} sm={4} xs={2}>
-          </Grid>
-          <Grid item lg={2} sm={4} xs={8}>
-            <Grid container direction="row" justifyContent="center" spacing={3} alignItems="stretch">
-              <Grid item xs={12}  >
-                <Button fullWidth className={classes.button} component={Link} to="/Example" color="primary" variant='contained'>  Example </Button>
-              </Grid>
+        <Grid item lg={4} sm={4} xs={2}></Grid>
+        <Grid item lg={2} sm={4} xs={8}>
+          <Grid
+            container
+            direction="row"
+            justifyContent="center"
+            spacing={3}
+            alignItems="stretch"
+          >
+            <Grid item xs={12}>
+              <Button
+                fullWidth
+                sx={{ mt: "10px", pt: "10px" }}
+                component={Link}
+                to="/Example"
+                color="primary"
+                variant="contained"
+              >
+                {" "}
+                Example{" "}
+              </Button>
             </Grid>
           </Grid>
-          <Grid item lg={4} sm={4} xs={2}>
-          </Grid>
         </Grid>
-      </TraditionalLayout>
-    )
-  }
-}
-
-export default withRouter(withStyles(styles)(Staging));
+        <Grid item lg={4} sm={4} xs={2}></Grid>
+      </Grid>
+    </TraditionalLayout>
+  );
+};
+export default Staging;

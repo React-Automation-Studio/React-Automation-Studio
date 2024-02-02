e46db46ed6a411ac526991557608ef46ff6874a8
William Duckitt <wdduckitt@sun.ac.za>
2024-02-02 09:50:42 +0200

    reconnect timeout was too long, causes an issue when an ioc is down, replaced it with a connect and check to see if the pv is connected

--- pvServer/pvServer.py
+++ pvServer/pvServer.py
@@ -294,7 +294,6 @@ def check_pv_initialized_after_disconnect():
             else:
                 if clientPVlist[pvname]["initialized"] == False:
                     if clientPVlist[pvname]["isConnected"]:
-                        clientPVlist[pvname]["connectRetries"]=0
                         clientPVlist[pvname]["pv"].get(as_string=True)
                         d = clientPVlist[pvname]["pv"].get_with_metadata(
                             with_ctrlvars=True, use_monitor=True
@@ -369,9 +368,9 @@ def check_pv_initialized_after_disconnect():
                                     log.exception("Unexpected error")
                                     raise
                     else:
-                        if clientPVlist[pvname]["connectRetries"]>1:    # wait at least 0.2 seconds before reconnecting
-                            clientPVlist[pvname]["pv"].reconnect()
-                        clientPVlist[pvname]["connectRetries"]+=1
+                        if clientPVlist[pvname]["pv"].connect(timeout=0.002):
+                            clientPVlist[pvname]["isConnected"] = True
+                        
         time.sleep(0.1)
 
 
@@ -695,7 +694,6 @@ def request_pv_info(message):
                 pvlist["pv"] = pv
                 pvlist["isConnected"] = False
                 pvlist["initialized"] = False
-                pvlist["connectRetries"] = 0
                 if "pvConnectionId" in message:
                     pvConnectionId = str(message["pvConnectionId"])
                 else:

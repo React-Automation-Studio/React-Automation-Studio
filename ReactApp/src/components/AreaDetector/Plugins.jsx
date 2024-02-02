38dace3eaef290f09e22ea042575210b6f7c856b
William Duckitt <wdduckitt@sun.ac.za>
2024-02-02 15:31:42 +0200

    Add IconButton to Plugins component

--- ReactApp/src/components/AreaDetector/Plugins.jsx
+++ ReactApp/src/components/AreaDetector/Plugins.jsx
@@ -9,8 +9,9 @@ import { useTheme } from "@mui/system";
 import { StyledTableCell } from "./TableStyles";
 import RadioButton from "../BaseComponents/RadioButton";
 import SettingsIcon from "@mui/icons-material/Settings";
+import IconButton from "@mui/material/IconButton";
 const Plugins = (props) => {
-  const {pluginTypes} = props;
+  const { pluginTypes } = props;
   return (
     <Table size="small">
       <TableHead>
@@ -29,66 +30,72 @@ const Plugins = (props) => {
       </TableHead>
       <TableBody>
         {pluginTypes.map((pluginType) => (
-        <TableRow key={pluginType}>
-          <TableCell align="left">
-          <TextUpdate
-              pv={`$(P)${pluginType}:PortName_RBV`}
-              macros={props.macros}
-            />
-          </TableCell>
-          <TableCell align="left">
-          <TextUpdate
-              pv={`$(P)${pluginType}:PluginType_RBV`}
-              macros={props.macros}
-            />
-          </TableCell>
-          <TableCell align="left">
-            <TextUpdate
-              pv={`$(P)${pluginType}:NDArrayPort`}
-              macros={props.macros}
-            />
-          </TableCell>
-          <TableCell align="left">
-            <RadioButton
-             pv={`$(P)${pluginType}:EnableCallbacks`}
-              macros={props.macros}
-            />
-          </TableCell>
-          <TableCell align="left">
-            <TextUpdate
-              pv={`$(P)${pluginType}:EnableCallbacks_RBV`}
-              macros={props.macros}
-              useStringValue
-            />
-          </TableCell>
-          <TableCell align="left">
-            <RadioButton
-             pv={`$(P)${pluginType}:BlockingCallbacks`}
-              macros={props.macros}
-            />
-          </TableCell>
-          <TableCell align="left">
-            <TextUpdate
-              pv={`$(P)${pluginType}:DroppedArrays_RBV`}
-              macros={props.macros}
-            />
-          </TableCell>
-          <TableCell align="left">
-            <TextUpdate
-              pv={`$(P)${pluginType}:QueueFree`}
-              macros={props.macros}
-            />
-          </TableCell>
-          <TableCell align="left">
-            <TextUpdate
-              pv={`$(P)${pluginType}:ArrayRate_RBV`}
-              macros={props.macros}
-            />
-          </TableCell>
-          <TableCell align="left">
-            <SettingsIcon/>
-          </TableCell>
-        </TableRow>
+          <TableRow key={pluginType}>
+            <TableCell align="left">
+              <TextUpdate
+                pv={`$(P)${pluginType}:PortName_RBV`}
+                macros={props.macros}
+              />
+            </TableCell>
+            <TableCell align="left">
+              <TextUpdate
+                pv={`$(P)${pluginType}:PluginType_RBV`}
+                macros={props.macros}
+              />
+            </TableCell>
+            <TableCell align="left">
+              <TextUpdate
+                pv={`$(P)${pluginType}:NDArrayPort`}
+                macros={props.macros}
+              />
+            </TableCell>
+            <TableCell align="left">
+              <RadioButton
+                pv={`$(P)${pluginType}:EnableCallbacks`}
+                macros={props.macros}
+              />
+            </TableCell>
+            <TableCell align="left">
+              <TextUpdate
+                pv={`$(P)${pluginType}:EnableCallbacks_RBV`}
+                macros={props.macros}
+                useStringValue
+              />
+            </TableCell>
+            <TableCell align="left">
+              <RadioButton
+                pv={`$(P)${pluginType}:BlockingCallbacks`}
+                macros={props.macros}
+              />
+            </TableCell>
+            <TableCell align="left">
+              <TextUpdate
+                pv={`$(P)${pluginType}:DroppedArrays_RBV`}
+                macros={props.macros}
+              />
+            </TableCell>
+            <TableCell align="left">
+              <TextUpdate
+                pv={`$(P)${pluginType}:QueueFree`}
+                macros={props.macros}
+              />
+            </TableCell>
+            <TableCell align="left">
+              <TextUpdate
+                pv={`$(P)${pluginType}:ArrayRate_RBV`}
+                macros={props.macros}
+              />
+            </TableCell>
+            <TableCell align="left">
+              <IconButton
+                onClick={() => {
+                  props.handleMoreOnClick(pluginType);
+                }}
+              >
+                <SettingsIcon />
+              </IconButton>
+            </TableCell>
+          </TableRow>
         ))}
       </TableBody>
     </Table>

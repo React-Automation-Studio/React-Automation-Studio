69e7ee45e01208008f0b87a8765ab87f41da3496
William Duckitt <william.duckitt@gmail.com>
2024-02-12 13:42:50 +0000

    cleaning up prod demo

--- dev.sh
+++
@@ -1,3 +0,0 @@
-docker rm $(docker ps -a -q) -f
-docker volume prune -f
-docker-compose -f docker-compose-dev.yml up --build
--- prod.sh
+++
@@ -1,3 +0,0 @@
-docker rm $(docker ps -a -q) -f
-docker volume prune -f
-docker-compose up --build

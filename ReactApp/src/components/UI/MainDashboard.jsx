8e9b2e60c03a0da8dca9d9e3c39061dc374f481c
William Duckitt <wdduckitt@sun.ac.za>
2024-03-08 09:58:13 +0200

    Update package versions and fix EPICS socketIO bug

--- ReactApp/src/components/UI/MainDashboard.jsx
+++ ReactApp/src/components/UI/MainDashboard.jsx
@@ -317,6 +317,16 @@ const MainDashboard = (props) => {
                 </Typography>
               </Grid>
               <Grid item lg={12} sm={12} xs={12}>
+              V5.0.1 Friday 08 March 2024 Minor Updates:
+                  <ul>
+                    <li>Package updates to Backend modules</li>
+                    <li>Updated to Epics 7.0.8 and Python 3.12.1</li>
+                    <li>Package updates to all Node modules</li>
+                    <li>Updated to the latest Node LTS</li>
+                    <li>Minor Bug fix to EPICS socketIO connections, PR#128</li>
+                  </ul>
+                 
+                  <br />
                 <Typography className={classes.WhatsNew} component="div">
                   V5.0.0 Friday 02 February 2024 Major Updates:
                   <ul>

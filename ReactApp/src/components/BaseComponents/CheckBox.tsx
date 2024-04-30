8f3eaaf2d64d289da3d0644ddc58a153986492b4
William Duckitt <wdduckitt@sun.ac.za>
2024-04-30 10:32:11 +0200

    Refactor CheckBox component props and defaultProps

--- ReactApp/src/components/BaseComponents/CheckBox.tsx
+++ ReactApp/src/components/BaseComponents/CheckBox.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Checkbox as MuiCheckBox, FormControlLabel } from "@mui/material";
 import Widget from "../SystemComponents/Widgets/Widget";
-import PropTypes from "prop-types";
 
 const CheckBoxComponent = (props) => {
   /**
@@ -41,11 +40,62 @@ const CheckBoxComponent = (props) => {
   );
 };
 
+interface CheckBoxProps {
+  /** Name of the process variable,  eg. '$(device):test$(id)'*/
+  pv: string;
+  /** Values of macros that will be substituted in the pv name eg. {{'$(device)':'testIOC','$(id)':'2'}}*/
+  macros?: object;
+
+  /** local variable initialization value*/
+  initialLocalVariableValue?: string;
+  /** If defined, then the DataConnection debugging information will be displayed*/
+  debug?: boolean;
+  /** label placement*/
+  labelPlacement?: "start" | "top" | "bottom" | "end";
+  /** Custom label to be used, if  `usePvLabel` is not defined. */
+  label?: string;
+  /**
+   * Custom on color to be used, must be derived from Material UI theme color's.
+   */
+  onColor?: string;
+  /**
+   * Directive to fill the component's label with
+   * the value contained in the  pv metadata's DESC field or the labelPv value.
+   * If not defined it uses the custom label as defined by the label prop.
+   */
+  usePvLabel?: boolean;
+  /**
+   * Custom PV to define the units to be used, usePvLabel must be set to `true` and useMetadata to `false`, eg. '$(device):test$(id)'.
+   */
+  labelPv?: string;
+  /** Any of the MUI Checkbox Props can applied by defining them as an object
+   *
+   */
+  muiCheckBoxProps?: object;
+  /**
+   * Tooltip Text
+   */
+  tooltip?: string;
+  /**
+   * Directive to show the tooltip
+   */
+  showTooltip?: boolean;
+  /**
+   *  Any of the MUI Tooltip props can applied by defining them as an object
+   */
+  tooltipProps?: object;
+}
+
 /**
  * The CheckBox component is a wrapper on a Material-UI CheckBox component.
  https://mui.com/api/checkbox/
  */
-const CheckBox = (props) => {
+const CheckBox = ({
+  onColor = "primary",
+  debug = false,
+  showTooltip = false,
+  ...props
+}: CheckBoxProps) => {
   return (
     <Widget
       {...props}
@@ -59,56 +109,6 @@ const CheckBox = (props) => {
   );
 };
 
-CheckBox.propTypes = {
-  /** Name of the process variable,  eg. '$(device):test$(id)'*/
-  pv: PropTypes.string.isRequired,
-  /** Values of macros that will be substituted in the pv name eg. {{'$(device)':'testIOC','$(id)':'2'}}*/
-  macros: PropTypes.object,
-
-  /** local variable initialization value*/
-  initialLocalVariableValue: PropTypes.string,
-  /** If defined, then the DataConnection debugging information will be displayed*/
-  debug: PropTypes.bool,
-  /** label placement*/
-  labelPlacement: PropTypes.oneOf(["start", "top", "bottom", "end"]),
-  /** Custom label to be used, if  `usePvLabel` is not defined. */
-  label: PropTypes.string,
-  /**
-   * Custom on color to be used, must be derived from Material UI theme color's.
-   */
-  onColor: PropTypes.string,
-  /**
-   * Directive to fill the component's label with
-   * the value contained in the  pv metadata's DESC field or the labelPv value.
-   * If not defined it uses the custom label as defined by the label prop.
-   */
-  usePvLabel: PropTypes.bool,
-  /**
-   * Custom PV to define the units to be used, usePvLabel must be set to `true` and useMetadata to `false`, eg. '$(device):test$(id)'.
-   */
-  labelPv: PropTypes.string,
-  /** Any of the MUI Checkbox Props can applied by defining them as an object
-   *
-   */
-  muiCheckBoxProps: PropTypes.object,
-  /**
-   * Tooltip Text
-   */
-  tooltip: PropTypes.string,
-  /**
-   * Directive to show the tooltip
-   */
-  showTooltip: PropTypes.bool,
-  /**
-   *  Any of the MUI Tooltip props can applied by defining them as an object
-   */
-  tooltipProps: PropTypes.object,
-};
-
-CheckBox.defaultProps = {
-  onColor: "primary",
-  debug: false,
-  showTooltip: false,
-};
+CheckBox.defaultProps = {};
 
 export default CheckBox;

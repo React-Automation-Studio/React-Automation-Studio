cb292e40ff9739a63eb5d3ace233fc355a92dcf6
William Duckitt <wdduckitt@sun.ac.za>
2024-05-02 11:14:13 +0200

    Refactor StyledIconIndicator component to use TypeScript and update props

--- ReactApp/src/components/BaseComponents/StyledIconIndicator.tsx
+++ ReactApp/src/components/BaseComponents/StyledIconIndicator.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { FormControlLabel, SvgIcon, useTheme } from "@mui/material";
 import Lens from "@mui/icons-material/Lens";
-import PropTypes from "prop-types";
 import Widget from "../SystemComponents/Widgets/Widget";
 
 function StyledIconIndicatorComponent(props) {
@@ -48,8 +47,8 @@ function StyledIconIndicatorComponent(props) {
   let color = !props.initialized
     ? theme.palette.action.disabled
     : props.value != 0
-    ? onColor
-    : offColor;
+      ? onColor
+      : offColor;
 
   iconStyle["color"] = color;
   /* eslint-enable eqeqeq */
@@ -86,61 +85,69 @@ function StyledIconIndicatorComponent(props) {
  * https://mui.com/api/svg-icon/<br/><br/>
  * A custom Icon can used by importing it in the parent and assigning it as a child <br/><br/>
  */
-const StyledIconIndicator = (props) => {
-  return <Widget {...props} component={StyledIconIndicatorComponent} />;
+const StyledIconIndicator = ({
+  onColor = "primary",
+  offColor = "default",
+  debug = false,
+  showTooltip = false,
+  ...props
+}: StyledIconIndicatorProps) => {
+  return (
+    <Widget
+      {...props}
+      component={StyledIconIndicatorComponent}
+      onColor={onColor}
+      offColor={offColor}
+      debug={debug}
+      showTooltip={showTooltip}
+    />
+  );
 };
 
-StyledIconIndicator.propTypes = {
+interface StyledIconIndicatorProps {
   /** Name of the process variable,  eg. '$(device):test$(id)'*/
-  pv: PropTypes.string.isRequired,
+  pv: string;
   /** Values of macros that will be substituted in the pv name eg. {{'$(device)':'testIOC','$(id)':'2'}}*/
-  macros: PropTypes.object,
+  macros?: object;
 
   /** local variable initialization value*/
-  initialLocalVariableValue: PropTypes.string,
+  initialLocalVariableValue?: string;
   /** If defined, then the DataConnection debugging information will be displayed*/
-  debug: PropTypes.bool,
+  debug?: boolean;
   /** label placement*/
-  labelPlacement: PropTypes.oneOf(["start", "top", "bottom", "end"]),
+  labelPlacement?: "start" | "top" | "bottom" | "end";
   /** Custom label to be used, if  `usePvLabel` is not defined. */
-  label: PropTypes.string,
+  label?: string;
   /**
    * Custom on color to be used, must be derived from Material UI theme color's or can be a standard color.
    */
-  onColor: PropTypes.string,
+  onColor?: string;
   /**
    * Custom off color to be used, must be derived from Material UI theme color's or can be a standard color.
    */
-  offColor: PropTypes.string,
+  offColor?: string;
   /**
    * Directive to fill the component's label with
    * the value contained in the  pv metadata's DESC field or the labelPv value.
    * If not defined it uses the custom label as defined by the label prop.
    */
-  usePvLabel: PropTypes.bool,
+  usePvLabel?: boolean;
   /**
    * Custom PV to define the units to be used, usePvLabel must be set to `true` and useMetadata to `false`, eg. '$(device):test$(id)'.
    */
-  labelPv: PropTypes.string,
+  labelPv?: string;
   /**
    * Tooltip Text
    */
-  tooltip: PropTypes.string,
+  tooltip?: string;
   /**
    * Directive to show the tooltip
    */
-  showTooltip: PropTypes.bool,
+  showTooltip?: boolean;
   /**
    *  Any of the MUI Tooltip props can applied by defining them as an object
    */
-  tooltipProps: PropTypes.object,
-};
-
-StyledIconIndicator.defaultProps = {
-  onColor: "primary",
-  offColor: "default",
-  debug: false,
-  showTooltip: false,
-};
+  tooltipProps?: object;
+}
 
 export default StyledIconIndicator;

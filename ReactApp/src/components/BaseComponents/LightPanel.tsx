60e38eda2cc1f5c3800f24162e9a4fa84bf9fc62
William Duckitt <wdduckitt@sun.ac.za>
2024-05-02 10:37:50 +0200

    Refactor LightPanel component to use TypeScript and add props

--- ReactApp/src/components/BaseComponents/LightPanel.tsx
+++ ReactApp/src/components/BaseComponents/LightPanel.tsx
@@ -121,102 +121,111 @@ function LightPanelComponent(props) {
  * The component is implemented with zero margins and enabled to grow to the width of its parent container.
  * The margins and spacing must be controlled from the parent component.<br/><br/>
  */
-const LightPanel = (props) => {
-  return <Widget {...props} component={LightPanelComponent} readOnly />;
+const LightPanel = ({
+  debug = false,
+  showTooltip = false,
+  useStringValue = true,
+  align = "center",
+  ...props
+}: LightPanelProps) => {
+  return (
+    <Widget
+      {...props}
+      component={LightPanelComponent}
+      readOnly
+      debug={debug}
+      showTooltip={showTooltip}
+      useStringValue={useStringValue}
+      align={align}
+    />
+  );
 };
 
-LightPanel.propTypes = {
+interface LightPanelProps {
   /** Name of the process variable,  eg. '$(device):test$(id)'*/
-  pv: PropTypes.string.isRequired,
+  pv: string;
 
   /** Values of macros that will be substituted in the pv name eg. {{'$(device)':'testIOC','$(id)':'2'}}*/
-  macros: PropTypes.object,
+  macros?: object;
 
   /** local variable initialization value*/
-  initialLocalVariableValue: PropTypes.string,
+  initialLocalVariableValue?: string;
 
   /** If defined, then the DataConnection debugging information will be displayed*/
-  debug: PropTypes.bool,
+  debug?: boolean;
 
   /** label placement*/
-  labelPlacement: PropTypes.oneOf(["start", "top", "bottom", "end"]),
+  labelPlacement?: "start" | "top" | "bottom" | "end";
 
   /** Custom label to be used, if  `usePvLabel` is not defined. */
-  label: PropTypes.string,
+  label?: string;
 
   /**
    * Custom colors to be used for each state. The colors must be derived from Material UI theme color's or can be a standard color.
    * This prop must be an object where the PV value corresponds to the key in the object,
    * and the value is the desired color. Eg: ``{0: "red", 1: "lime"}``.
    */
-  colors: PropTypes.object,
+  colors?: object;
 
   /**
    * Color corresponding to PV value == ``1``. When the colors prop is defined, this will be ignored.
    */
-  onColor: PropTypes.string,
+  onColor?: string;
 
   /**
    * Color corresponding to PV value == ``0``. When the colors prop is defined, this will be ignored.
    */
-  offColor: PropTypes.string,
+  offColor?: string;
 
   /** Show the PV string value instead of the numeric one.*/
-  useStringValue: PropTypes.bool,
+  useStringValue?: boolean;
 
   /** Custom strings to display as values. The integer value of the PV is the index of the array to display.
    * `useStringValue` must be `false`.
    */
-  customValueStrings: PropTypes.arrayOf(PropTypes.string),
+  customValueStrings?: string[];
 
   /**
    * Directive to fill the component's label with
    * the value contained in the  pv metadata's DESC field or the labelPv value.
    * If not defined it uses the custom label as defined by the label prop.
    */
-  usePvLabel: PropTypes.bool,
+  usePvLabel?: boolean;
 
   /**
    * Custom PV to define the units to be used, usePvLabel must be set to `true` and useMetadata to `false`, eg. '$(device):test$(id)'.
    */
-  labelPv: PropTypes.string,
+  labelPv?: string;
 
   /**
    * Tooltip Text
    */
-  tooltip: PropTypes.string,
+  tooltip?: string;
 
   /**
    * Directive to show the tooltip
    */
-  showTooltip: PropTypes.bool,
+  showTooltip?: boolean;
 
   /**
    *  Any of the MUI Tooltip props can applied by defining them as an object
    */
-  tooltipProps: PropTypes.object,
+  tooltipProps?: object;
 
   /**
    * Any of the MUI TextField Props can applied by defining them as an object.
    */
-  muiTypographyProps: PropTypes.object,
+  muiTypographyProps?: object;
 
   /**
    * Material UI Typography align. Used to align the value string inside the LightPanel.
    */
-  align: PropTypes.string,
+  align?: string;
 
   /**
    * Material UI Typography variant. Used to change the style of the value string inside the LightPanel.
    */
-  variant: PropTypes.string,
-};
-
-LightPanel.defaultProps = {
-  debug: false,
-  showTooltip: false,
-  useStringValue: true,
-  align: "center",
-};
+  variant?: string;
+}
 
 export default LightPanel;

289a51961a377cc21a3dff581b906875a943172b
William Duckitt <wdduckitt@sun.ac.za>
2024-05-02 12:38:26 +0200

    Update TextUpdateStateful component to remove unused import

--- ReactApp/src/components/AlarmHandler/TextUpdateStateful.tsx
+++ ReactApp/src/components/AlarmHandler/TextUpdateStateful.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import withStyles from "@mui/styles/withStyles";
 import Widget from "../SystemComponents/Widgets/Widget";
 import { Typography } from "@mui/material";
-import PropTypes from "prop-types";
 
 const styles = (theme) => ({
   disconAlarmWarn: {

9104ea3803fdae763666e4c8b39e9dc7b04dc195
William Duckitt <wdduckitt@sun.ac.za>
2024-05-15 15:49:48 +0200

    chore: Update Login version to V5.2.0

--- ReactApp/src/AppRoutes.jsx
+++ ReactApp/src/AppRoutes.jsx
@@ -74,7 +74,7 @@ const AppRoutes = (props) => {
           <Route
             exact
             path="/Login"
-            element={<Login version="V5.1.0" timeout={5000} />}
+            element={<Login version="V5.2.0" timeout={5000} />}
           />
         )}
 

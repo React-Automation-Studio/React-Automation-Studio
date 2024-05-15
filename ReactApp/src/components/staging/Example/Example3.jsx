ec1ddddfd0fe11e5cd641939b495e8a56a0f4e58
William Duckitt <wdduckitt@sun.ac.za>
2024-05-15 15:50:30 +0200

    Restore example to the demo state

--- ReactApp/src/components/staging/Example/Example3.jsx
+++ ReactApp/src/components/staging/Example/Example3.jsx
@@ -1,23 +1,10 @@
 import React from "react";
 import SideBar from "../../../components/SystemComponents/SideBar";
-import BitIndicators from "../../../components/BaseComponents/BitIndicators";
 const Example3 = () => {
   return (
     <React.Fragment>
       <SideBar />
       <div> Hello World</div>
-      <BitIndicators
-        bitLabelPlacement="end"
-        label="Bits"
-        labelPlacement="top"
-        macros={{
-          "$(device)": "testIOC",
-          "$(id)": "2",
-        }}
-        pv="$(device):test$(id)"
-        onColor="yellow"
-        offColor="purple"
-      />
     </React.Fragment>
   );
 };

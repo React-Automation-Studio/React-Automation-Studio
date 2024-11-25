00402c82f93aeb024aee9289aeaf95d8f0735f15
William Duckitt <wdduckitt@sun.ac.za>
2024-11-25 13:00:44 +0200

    refactor GraphY stories to improve code formatting and add new data point scenarios; enhance readability and maintainability

--- ReactApp/src/components/BaseComponents/GraphY.stories.tsx
+++ ReactApp/src/components/BaseComponents/GraphY.stories.tsx
@@ -14,11 +14,15 @@ export default {
 const Template: StoryObj = {
   render: ({ ...args }) => {
     return (
-      <div >
-        <GraphY {...args} style={{height:'25vh'}} />
+      <div>
+        <GraphY {...args} style={{ height: "25vh" }} />
         <br />
 
-        <Slider  pv='testIOC:amplitude'   label='Sine Wave Amplitude' usePvMinMax={true}/>
+        <Slider
+          pv="testIOC:amplitude"
+          label="Sine Wave Amplitude"
+          usePvMinMax={true}
+        />
       </div>
     );
   },
@@ -27,18 +31,34 @@ const Template: StoryObj = {
 export const Overview = {
   ...Template,
   args: {
-    pvs:['testIOC:test4','testIOC:test5'], 
-    legend:['Modulated Sine Wave Amplitude','Sine Wave Amplitude']
-    
+    pvs: ["testIOC:test4", "testIOC:test5"],
+    legend: ["Modulated Sine Wave Amplitude", "Sine Wave Amplitude"],
   },
 };
 
 export const AlternateColors = {
   ...Template,
   args: {
-    pvs:['testIOC:test4','testIOC:test5'], 
-    legend:['Modulated Sine Wave Amplitude','Sine Wave Amplitude'],
-    lineColor:['red','green']
-    
+    pvs: ["testIOC:test4", "testIOC:test5"],
+    legend: ["Modulated Sine Wave Amplitude", "Sine Wave Amplitude"],
+    lineColor: ["red", "green"],
+  },
+};
+
+export const OneHundredThousandDataPoints = {
+  ...Template,
+  args: {
+    pvs: ["testIOC:test4", "testIOC:test5"],
+    legend: ["Modulated Sine Wave Amplitude", "Sine Wave Amplitude"],
+    maxLength: 100000,
+  },
+};
+
+export const OneMillionDataPoints = {
+  ...Template,
+  args: {
+    pvs: ["testIOC:test4", "testIOC:test5"],
+    legend: ["Modulated Sine Wave Amplitude", "Sine Wave Amplitude"],
+    maxLength: 1000000,
   },
 };

c254819e6705fdac21a75a07e229a74c776adbe9
William Duckitt <wdduckitt@sun.ac.za>
2025-07-08 23:32:39 +0200

    refactor: convert HarpRangeSelection to functional component and remove withStyles HOC

--- ReactApp/src/components/SiteSpecificComponents/iThembaLABS/CompoundComponents/HarpRangeSelection.jsx
+++ ReactApp/src/components/SiteSpecificComponents/iThembaLABS/CompoundComponents/HarpRangeSelection.jsx
@@ -1,89 +1,70 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import AutomationStudioContext from "../../../SystemComponents/AutomationStudioContext";
 import DataConnection from "../../../SystemComponents/DataConnection";
-import withStyles from "@mui/styles/withStyles";
+import { useTheme } from "@mui/material/styles";
 
 import TextField from "@mui/material/TextField";
 
 import MenuItem from "@mui/material/MenuItem";
 
-/* eslint-disable eqeqeq */
-const styles = (theme) => ({
-  TextField: {
-    width: "100%",
-    marginLeft: "auto",
-    marginRight: "auto",
-    paddingBottom: 0,
-    marginTop: 0,
-    fontWeight: 500,
-    borderRadius: 4,
-  },
-});
+const HarpRangeSelection = (props) => {
+  const context = useContext(AutomationStudioContext);
+  const theme = useTheme();
 
-class HarpRangeSelection extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      xrange: {
-        inputValue: 1,
-        initialized: false,
-        pvname: "",
-        severity: 0,
-        metadata: {},
-      },
-      yrange: { inputValue: 1, initialized: false, pvname: "", severity: 0 },
-    };
-    this.handleInputValue = this.handleInputValue.bind(this);
-    this.handleInputValueLabel = this.handleInputValueLabel.bind(this);
-    this.handleMetadata = this.handleMetadata.bind(this);
-  }
+  const [state, setState] = useState({
+    xrange: {
+      inputValue: 1,
+      initialized: false,
+      pvname: "",
+      severity: 0,
+      metadata: {},
+    },
+    yrange: { inputValue: 1, initialized: false, pvname: "", severity: 0 },
+  });
 
-  handleInputValue = (name) => (inputValue, pvname, initialized, severity) => {
-    let pv = this.state[name];
+  const handleInputValue = (name) => (inputValue, pvname, initialized, severity) => {
+    let pv = state[name];
     pv.inputValue = inputValue;
     pv.pvname = pvname;
     pv.initialized = initialized;
     pv.severity = severity;
 
-    this.setState({ [name]: pv });
+    setState(prev => ({ ...prev, [name]: pv }));
   };
 
-  handleMetadata = (name) => (metadata) => {
-    let pv = this.state[name];
+  const handleMetadata = (name) => (metadata) => {
+    let pv = state[name];
     pv.metadata = metadata;
-    this.setState({ [name]: pv });
+    setState(prev => ({ ...prev, [name]: pv }));
   };
 
-  handleInputValueLabel(inputValue) {
-    this.setState({ label: inputValue });
-  }
-
-  componentDidMount() {}
-
-  componentWillUnmount() {}
-
-  handleOnFocus = (event) => {
-    this.setState({ hasFocus: true });
+  const handleInputValueLabel = (inputValue) => {
+    setState(prev => ({ ...prev, label: inputValue }));
   };
 
-  catchReturn = (stateVar) => (event) => {
+  const handleOnFocus = (event) => {
+    setState(prev => ({ ...prev, hasFocus: true }));
+  };
+
+  const catchReturn = (stateVar) => (event) => {
     if (event.key === "Enter") {
-      this.setState({ outputValue: this.state.value });
+      setState(prev => ({ ...prev, outputValue: prev.value }));
     }
   };
 
-  handleOnBlur = (event) => {
-    this.setState({
+  const handleOnBlur = (event) => {
+    setState(prev => ({
+      ...prev,
       hasFocus: false,
-      value: this.state["inputValue"],
-      metadata: this.state["newMetadata"],
-    });
+      value: prev["inputValue"],
+      metadata: prev["newMetadata"],
+    }));
   };
 
-  handleChange = (name) => (event) => {
+  const handleChange = (name) => (event) => {
     console.log(event.target.value);
-    let xRangePV = this.state["xrange"];
-    let yRangePV = this.state["yrange"];
+    let xRangePV = state["xrange"];
+    let yRangePV = state["yrange"];
     let value;
     switch (event.target.value) {
       case " 20 -  200 uA":
@@ -110,130 +91,134 @@ class HarpRangeSelection extends React.Component {
     console.log(value);
     xRangePV.inputValue = value;
     yRangePV.inputValue = value;
-    this.setState({
+    setState(prev => ({
+      ...prev,
       xrange: xRangePV,
       yrange: yRangePV,
-    });
+    }));
   };
 
-  render() {
-    let yrangePV;
-    if (this.props.onlyX === true) {
-      yrangePV = this.props.systemName + ":xrange";
-    } else {
-      yrangePV = this.props.systemName + ":yrange";
-    }
-
-    let xrangePV;
-    if (this.props.onlyY === true) {
-      xrangePV = this.props.systemName + ":yrange";
-    } else {
-      xrangePV = this.props.systemName + ":xrange";
-    }
-
-    const { classes } = this.props;
-
-    const initialized =
-      this.state.xrange.initialized && this.state.yrange.initialized;
-
-    let value = this.state.xrange.inputValue;
-    let enum_strings = {};
-
-    if (initialized) {
-      switch (parseInt(this.state.xrange.inputValue)) {
-        case 1:
-          value = " 20 -  200 uA";
-          break;
-        case 2:
-          value = "  2 -   20 uA";
-          break;
-        case 3:
-          value = "200 - 2000 nA";
-          break;
-        case 4:
-          value = " 20 -  200 nA";
-          break;
-        case 5:
-          value = "  2 -   20 nA";
-          break;
-        case 6:
-          value = "200 - 2000 pA";
-          break;
-      }
-    }
-
-    enum_strings = [
-      " 20 -  200 uA",
-      "  2 -   20 uA",
-      "200 - 2000 nA",
-      " 20 -  200 nA",
-      "  2 -   20 nA",
-      "200 - 2000 pA",
-    ];
-
-    let write_access = false;
-
-    if (initialized) {
-      if (typeof this.state.xrange.metadata !== "undefined") {
-        if (typeof this.state.xrange.metadata.write_access !== "undefined") {
-          write_access = this.state.xrange.metadata.write_access;
-        }
-      }
-    }
-
-    return (
-      <React.Fragment>
-        <DataConnection
-          pv={xrangePV}
-          handleInputValue={this.handleInputValue("xrange")}
-          handleMetadata={this.handleMetadata("xrange")}
-          outputValue={this.state["xrange"].inputValue}
-        />
-        <DataConnection
-          pv={yrangePV}
-          handleInputValue={this.handleInputValue("yrange")}
-          handleMetadata={this.handleMetadata("yrange")}
-          outputValue={this.state["yrange"].inputValue}
-        />
-
-        {initialized === true && (
-          <TextField
-            disabled={write_access === false ? true : false}
-            select
-            className={classes.TextField}
-            value={value}
-            onKeyPress={this.catchReturn("value")}
-            onFocus={(event) => this.handleOnFocus(event)}
-            onBlur={(event) => this.handleOnBlur(event)}
-            onChange={this.handleChange("value")}
-            label={this.props.label}
-            margin="normal"
-            variant="outlined"
-          >
-            {enum_strings.map((item, index) => (
-              <MenuItem key={item.toString()} value={item}>
-                {" "}
-                {item}{" "}
-              </MenuItem>
-            ))}
-          </TextField>
-        )}
-
-        {(initialized === false || initialized === "undefined") && (
-          <TextField
-            value={""}
-            label={"Connecting to:" + xrangePV}
-            disabled={true}
-            margin="normal"
-            variant="outlined"
-          />
-        )}
-      </React.Fragment>
-    );
+  let yrangePV;
+  if (props.onlyX === true) {
+    yrangePV = props.systemName + ":xrange";
+  } else {
+    yrangePV = props.systemName + ":yrange";
   }
-}
 
-HarpRangeSelection.contextType = AutomationStudioContext;
-export default withStyles(styles)(HarpRangeSelection);
+  let xrangePV;
+  if (props.onlyY === true) {
+    xrangePV = props.systemName + ":yrange";
+  } else {
+    xrangePV = props.systemName + ":xrange";
+  }
+
+  const initialized =
+    state.xrange.initialized && state.yrange.initialized;
+
+  let value = state.xrange.inputValue;
+  let enum_strings = {};
+
+  if (initialized) {
+    switch (parseInt(state.xrange.inputValue)) {
+      case 1:
+        value = " 20 -  200 uA";
+        break;
+      case 2:
+        value = "  2 -   20 uA";
+        break;
+      case 3:
+        value = "200 - 2000 nA";
+        break;
+      case 4:
+        value = " 20 -  200 nA";
+        break;
+      case 5:
+        value = "  2 -   20 nA";
+        break;
+      case 6:
+        value = "200 - 2000 pA";
+        break;
+    }
+  }
+
+  enum_strings = [
+    " 20 -  200 uA",
+    "  2 -   20 uA",
+    "200 - 2000 nA",
+    " 20 -  200 nA",
+    "  2 -   20 nA",
+    "200 - 2000 pA",
+  ];
+
+  let write_access = false;
+
+  if (initialized) {
+    if (typeof state.xrange.metadata !== "undefined") {
+      if (typeof state.xrange.metadata.write_access !== "undefined") {
+        write_access = state.xrange.metadata.write_access;
+      }
+    }
+  }
+
+  return (
+    <React.Fragment>
+      <DataConnection
+        pv={xrangePV}
+        handleInputValue={handleInputValue("xrange")}
+        handleMetadata={handleMetadata("xrange")}
+        outputValue={state["xrange"].inputValue}
+      />
+      <DataConnection
+        pv={yrangePV}
+        handleInputValue={handleInputValue("yrange")}
+        handleMetadata={handleMetadata("yrange")}
+        outputValue={state["yrange"].inputValue}
+      />
+
+      {initialized === true && (
+        <TextField
+          disabled={write_access === false ? true : false}
+          select
+          sx={{
+            width: "100%",
+            marginLeft: "auto",
+            marginRight: "auto",
+            paddingBottom: 0,
+            marginTop: 0,
+            fontWeight: 500,
+            borderRadius: 4,
+          }}
+          value={value}
+          onKeyPress={catchReturn("value")}
+          onFocus={(event) => handleOnFocus(event)}
+          onBlur={(event) => handleOnBlur(event)}
+          onChange={handleChange("value")}
+          label={props.label}
+          margin="normal"
+          variant="outlined"
+        >
+          {enum_strings.map((item, index) => (
+            <MenuItem key={item.toString()} value={item}>
+              {" "}
+              {item}{" "}
+            </MenuItem>
+          ))}
+        </TextField>
+      )}
+
+      {(initialized === false || initialized === "undefined") && (
+        <TextField
+          value={""}
+          label={"Connecting to:" + xrangePV}
+          disabled={true}
+          margin="normal"
+          variant="outlined"
+        />
+      )}
+    </React.Fragment>
+  );
+};
+
+export default HarpRangeSelection;
 
 /* eslint-disable eqeqeq */

594ae0955e6f0a086901b3788122c7f08ff6e48d
William Duckitt <wdduckitt@sun.ac.za>
2026-05-06 00:23:24 +0200

    improving testing

--- ReactApp/src/components/BaseComponents/TextInput.stories.js
+++ ReactApp/src/components/BaseComponents/TextInput.stories.js
@@ -1,16 +1,17 @@
 import  TextInput  from './TextInput';
+import { within, userEvent, expect, waitFor } from 'storybook/test';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   component: TextInput,
   parameters: {
-   
+
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ['autodocs'],
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
-  
+
   },
 };
 
@@ -24,7 +25,45 @@ export const Primary = {
        usePvUnits:true,
        usePvMinMax:true,
        alarmSensitive:true,
-    
+
+  },
+};
+
+// Integration test: writes a value through pvServer to the demo IOC and asserts
+// the new value comes back via the PV update. Exercises socket.io -> pvServer ->
+// PyEpics -> testIOC -> pvServer -> client roundtrip.
+export const IocRoundtripTest = {
+  args: {
+    pv: '$(device):test$(id)',
+    macros: { '$(device)': 'testIOC', '$(id)': '2' },
+    usePvLabel: true,
+    usePvPrecision: true,
+    usePvUnits: true,
+    usePvMinMax: true,
+    alarmSensitive: true,
+  },
+  parameters: { docs: { disable: true } },
+  tags: ['!autodocs'],
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const input = await waitFor(() => canvas.getByRole('textbox'), { timeout: 15000 });
+    await waitFor(() => expect(input).not.toBeDisabled(), { timeout: 15000 });
+
+    await userEvent.clear(input);
+    await userEvent.type(input, '42{Enter}');
+
+    await waitFor(
+      () => expect(parseFloat(input.value)).toBe(42),
+      { timeout: 5000 },
+    );
+
+    // Reset to a known value so subsequent runs/users see a sane state
+    await userEvent.clear(input);
+    await userEvent.type(input, '5000{Enter}');
+    await waitFor(
+      () => expect(parseFloat(input.value)).toBe(5000),
+      { timeout: 5000 },
+    );
   },
 };
 
